fbb639eed5d0a0a64087fe9ea2e4fff1d68c84e8
Tobias Wood <tobias@spinicist.org.uk>
2021-02-23 12:37:12 +0000

    Forgot bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,5 @@
 git submodule update --init --recursive
 cmake -B build -S . \
     -GNinja -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_TOOLCHAIN_FILE=cmake/Sanitizers.cmake
+    -DCMAKE_TOOLCHAIN_FILE=cmake/x64-osx-native.toolchain.cmake
 cmake --build build
