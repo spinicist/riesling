29f6621022820c112162a4d455592e31c142c6b5
Tobias Wood <tobias@spinicist.org.uk>
2021-03-03 20:54:29 +0000

    Incorrect transpose location in compressor

--- src/compressor.cpp
+++ src/compressor.cpp
@@ -11,46 +11,38 @@ Compressor::Compressor(Cx3 const &ks, long const nc, Log &log)
   Eigen::SelfAdjointEigenSolver<Eigen::MatrixXcf> eig(gramian);
   Eigen::ArrayXf vals = eig.eigenvalues().reverse().array().abs();
   vals /= vals.abs().sum();
-  long const n = std::min(nc, ks.dimension(0));
+  long const n = std::max(std::min(nc, ks.dimension(0)), 1L);
   log.info(
       FMT_STRING("PCA Compression Retaining {} virtual coils, total energy {}%"),
       n,
       100.f * vals.head(n).sum());
-  psi_ = eig.eigenvectors().rightCols(n).rowwise().reverse().transpose();
+  psi_ = eig.eigenvectors().rightCols(n).rowwise().reverse();
 }
 
 long Compressor::out_channels() const
 {
-  return psi_.rows();
+  return psi_.cols();
 }
 
 void Compressor::compress(Cx3 const &source, Cx3 &dest)
 {
-  if (psi_.size() != 0) {
-    assert(source.dimension(0) == psi_.rows());
-    assert(dest.dimension(0) == psi_.cols());
-    log_.info(FMT_STRING("Applying coil compression"));
-    auto const sourcemat = CollapseToMatrix(source);
-    auto destmat = CollapseToMatrix(dest);
-    destmat.noalias() = psi_ * sourcemat;
-  } else {
-    dest = source;
-  }
+  assert(source.dimension(0) == psi_.rows());
+  assert(dest.dimension(0) == psi_.cols());
+  log_.info(FMT_STRING("Applying coil compression"));
+  auto const sourcemat = CollapseToMatrix(source);
+  auto destmat = CollapseToMatrix(dest);
+  destmat.noalias() = psi_ * sourcemat;
 }
 
 void Compressor::compress(Cx4 const &source, Cx4 &dest)
 {
-  if (psi_.size() != 0) {
-    assert(source.dimension(1) == dest.dimension(1));
-    assert(source.dimension(2) == dest.dimension(2));
-    assert(source.dimension(3) == dest.dimension(3));
-    assert(source.dimension(0) == psi_.rows());
-    assert(dest.dimension(0) == psi_.cols());
-    log_.info(FMT_STRING("Applying coil compression"));
-    auto const sourcemat = CollapseToMatrix(source);
-    auto destmat = CollapseToMatrix(dest);
-    destmat.noalias() = psi_ * sourcemat;
-  } else {
-    dest = source;
-  }
+  assert(source.dimension(1) == dest.dimension(1));
+  assert(source.dimension(2) == dest.dimension(2));
+  assert(source.dimension(3) == dest.dimension(3));
+  assert(source.dimension(0) == psi_.rows());
+  assert(dest.dimension(0) == psi_.cols());
+  log_.info(FMT_STRING("Applying coil compression"));
+  auto const sourcemat = CollapseToMatrix(source);
+  auto destmat = CollapseToMatrix(dest);
+  destmat.noalias() = psi_.transpose() * sourcemat;
 }
