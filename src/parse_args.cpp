e93f711e5eb4d08f9229e64821ca0d00a6ebcd25
Emil Ljungberg <ljungberg.emil@gmail.com>
2021-04-21 15:12:10 +0100

    Improved split cmd and h5 saving

--- src/parse_args.cpp
+++ src/parse_args.cpp
@@ -1,7 +1,7 @@
-#include "io_nifti.h"
 #include "parse_args.h"
-#include "threads.h"
 #include "io_hd5.h"
+#include "io_nifti.h"
+#include "threads.h"
 #include <algorithm>
 #include <filesystem>
 #include <fmt/format.h>
@@ -12,8 +12,8 @@ std::unordered_map<int, Log::Level> levelMap{
     {0, Log::Level::Fail}, {1, Log::Level::Info}, {2, Log::Level::Images}, {3, Log::Level::Debug}};
 } // namespace
 
-void Vector3fReader::operator()(
-    std::string const &name, std::string const &value, Eigen::Vector3f &v)
+void Vector3fReader::
+operator()(std::string const &name, std::string const &value, Eigen::Vector3f &v)
 {
   float x, y, z;
   auto result = scn::scan(scn::make_view(value), "{},{},{}", x, y, z);
@@ -105,15 +105,14 @@ void WriteVolumes(
   T const out = vols.slice(st, sz);
 
   std::string ext = std::filesystem::path(fname).extension();
-  if (ext.compare("h5")==0){
+  if (ext.compare(".h5") == 0) {
     HD5Writer writer(fname, log);
     writer.writeInfo(info);
-    for (long iv=0; iv < out.dimension(3); iv++) {
-      Cx3 h5out = out.chip(iv,3).template cast<Cx>();
+    for (long iv = 0; iv < out.dimension(3); iv++) {
+      Cx3 h5out = out.chip(iv, 3).template cast<Cx>();
       writer.writeData(iv, h5out);
     }
-  }
-  else {
+  } else {
     WriteNifti(info, out, fname, log);
   }
 }
