ea17f08cbb62ba8daeae742b16997cc1a7ebb95a
Tobias Wood <tobias@mi3.com>
2022-01-26 16:37:02 +0000

    Add size 4 / 6 kernels

--- bench/grid.cpp
+++ bench/grid.cpp
@@ -6,80 +6,105 @@
 
 #include <catch2/catch.hpp>
 
-TEST_CASE("Grid")
+Index const M = 32;
+Index const C = 8;
+Info const info{
+  .type = Info::Type::ThreeD,
+  .matrix = Eigen::Array3l::Constant(M),
+  .channels = C,
+  .read_points = M / 2,
+  .spokes = M * M,
+  .volumes = 1,
+  .echoes = 1,
+  .tr = 1.f,
+  .voxel_size = Eigen::Array3f::Constant(1.f),
+  .origin = Eigen::Array3f::Constant(0.f),
+  .direction = Eigen::Matrix3f::Identity()};
+
+auto nc = info.noncartesianVolume();
+
+auto const points = ArchimedeanSpiral(info.read_points, info.spokes);
+Trajectory traj(info, points);
+float const os = 2.f;
+auto const m1 = traj.mapping(1, os);
+auto const m3 = traj.mapping(3, os);
+auto const m4 = traj.mapping(4, os);
+auto const m5 = traj.mapping(5, os);
+auto const m6 = traj.mapping(6, os);
+auto const nn = make_kernel("NN", info.type, os);
+auto const kb3 = make_kernel("KB3", info.type, os);
+auto const kb4 = make_kernel("KB4", info.type, os);
+auto const kb5 = make_kernel("KB5", info.type, os);
+auto const kb6 = make_kernel("KB6", info.type, os);
+
+TEST_CASE("GridEcho")
 {
-  Index const M = 32;
-  Index const C = 8;
-  Info const info{
-    .type = Info::Type::ThreeD,
-    .matrix = Eigen::Array3l::Constant(M),
-    .channels = C,
-    .read_points = M / 2,
-    .spokes = M * M,
-    .volumes = 1,
-    .echoes = 1,
-    .tr = 1.f,
-    .voxel_size = Eigen::Array3f::Constant(1.f),
-    .origin = Eigen::Array3f::Constant(0.f),
-    .direction = Eigen::Matrix3f::Identity()};
-  auto const points = ArchimedeanSpiral(info.read_points, info.spokes);
-  Trajectory traj(info, points);
-
-  float const os = 2.f;
-
-  BENCHMARK("Mapping")
-  {
-    traj.mapping(1, os);
-  };
-
-  auto const nn = make_kernel("NN", info.type, os);
-  auto const kb = make_kernel("KB5", info.type, os);
-
-  auto const m1 = traj.mapping(1, os);
-  auto const m5 = traj.mapping(5, os);
-
   auto gridnn = make_grid(nn.get(), m1, false);
-  auto gridkb = make_grid(kb.get(), m5, false);
-
-  auto nc = info.noncartesianVolume();
-  Index const nB = 4;
-  R2 basis(256, nB);
-  basis.setConstant(1.f);
-
-  auto gridbnn = make_grid_basis(nn.get(), m1, basis, false);
-  auto gridbkb = make_grid_basis(kb.get(), m5, basis, false);
+  auto gridkb3 = make_grid(kb3.get(), m3, false);
+  auto gridkb4 = make_grid(kb4.get(), m4, false);
+  auto gridkb5 = make_grid(kb5.get(), m5, false);
+  auto gridkb6 = make_grid(kb6.get(), m6, false);
 
   BENCHMARK("NN Noncartesian->Cartesian")
   {
     gridnn->Adj(nc);
   };
-  BENCHMARK("NN Basis Noncartesian->Cartesian")
+
+  BENCHMARK("KB3 Noncartesian->Cartesian")
   {
-    gridbnn->Adj(nc);
-  };
-  BENCHMARK("KB Noncartesian->Cartesian")
-  {
-    gridkb->Adj(nc);
-  };
-  BENCHMARK("KB Basis Noncartesian->Cartesian")
-  {
-    gridbkb->Adj(nc);
+    gridkb3->Adj(nc);
   };
 
-  BENCHMARK("NN Cartesian->Noncartesian")
+  BENCHMARK("KB4 Noncartesian->Cartesian")
   {
-    nc = gridnn->A();
+    gridkb4->Adj(nc);
   };
-  BENCHMARK("KB Cartesian->Noncartesian")
+
+  BENCHMARK("KB5 Noncartesian->Cartesian")
   {
-    nc = gridkb->A();
+    gridkb5->Adj(nc);
   };
-  BENCHMARK("NN Basis Cartesian->Noncartesian")
+
+  BENCHMARK("KB6 Noncartesian->Cartesian")
   {
-    nc = gridbnn->A();
+    gridkb6->Adj(nc);
   };
-  BENCHMARK("KB Basis Cartesian->Noncartesian")
+}
+
+TEST_CASE("GridBasis")
+{
+  Index const nB = 4;
+  R2 basis(256, nB);
+  basis.setConstant(1.f);
+
+  auto gridnn = make_grid_basis(nn.get(), m1, basis, false);
+  auto gridkb3 = make_grid_basis(kb3.get(), m3, basis, false);
+  auto gridkb4 = make_grid_basis(kb4.get(), m4, basis, false);
+  auto gridkb5 = make_grid_basis(kb5.get(), m5, basis, false);
+  auto gridkb6 = make_grid_basis(kb6.get(), m6, basis, false);
+
+  BENCHMARK("NN Noncartesian->Cartesian")
   {
-    nc = gridbkb->A();
+    gridnn->Adj(nc);
+  };
+
+  BENCHMARK("KB3 Noncartesian->Cartesian")
+  {
+    gridkb3->Adj(nc);
+  };
+
+  BENCHMARK("KB4 Noncartesian->Cartesian")
+  {
+    gridkb4->Adj(nc);
+  };
+
+  BENCHMARK("KB5 Noncartesian->Cartesian")
+  {
+    gridkb5->Adj(nc);
+  };
+
+  BENCHMARK("KB6 Noncartesian->Cartesian")
+  {
+    gridkb6->Adj(nc);
   };
 }
--- src/op/grid.cpp
+++ src/op/grid.cpp
@@ -11,23 +11,31 @@ std::unique_ptr<GridBase> make_grid(Kernel const *k, Mapping const &m, bool cons
   case 1:
     return std::make_unique<GridEcho<1, 1>>(dynamic_cast<SizedKernel<1, 1> const *>(k), m, fg);
   case 3:
-    switch (k->inPlane()) {
-    case 1:
+    if (k->throughPlane() == 1) {
       return std::make_unique<GridEcho<3, 1>>(dynamic_cast<SizedKernel<3, 1> const *>(k), m, fg);
-    case 3:
+    } else {
       return std::make_unique<GridEcho<3, 3>>(dynamic_cast<SizedKernel<3, 3> const *>(k), m, fg);
     }
-    __builtin_unreachable();
+  case 4:
+    if (k->throughPlane() == 1) {
+      return std::make_unique<GridEcho<4, 1>>(dynamic_cast<SizedKernel<4, 1> const *>(k), m, fg);
+    } else {
+      return std::make_unique<GridEcho<4, 4>>(dynamic_cast<SizedKernel<4, 4> const *>(k), m, fg);
+    }
   case 5:
-    switch (k->inPlane()) {
-    case 1:
+    if (k->throughPlane() == 1) {
       return std::make_unique<GridEcho<5, 1>>(dynamic_cast<SizedKernel<5, 1> const *>(k), m, fg);
-    case 5:
+    } else {
       return std::make_unique<GridEcho<5, 5>>(dynamic_cast<SizedKernel<5, 5> const *>(k), m, fg);
     }
-    __builtin_unreachable();
+  case 6:
+    if (k->throughPlane() == 1) {
+      return std::make_unique<GridEcho<6, 1>>(dynamic_cast<SizedKernel<6, 1> const *>(k), m, fg);
+    } else {
+      return std::make_unique<GridEcho<6, 6>>(dynamic_cast<SizedKernel<6, 6> const *>(k), m, fg);
+    }
   }
-  __builtin_unreachable();
+  Log::Fail(FMT_STRING("No grids implemented for in-plane kernel width {}"), k->inPlane());
 }
 
 std::unique_ptr<GridBase>
@@ -37,25 +45,37 @@ make_grid_basis(Kernel const *k, Mapping const &m, R2 const &b, bool const fg)
   case 1:
     return std::make_unique<GridBasis<1, 1>>(dynamic_cast<SizedKernel<1, 1> const *>(k), m, b, fg);
   case 3:
-    switch (k->inPlane()) {
-    case 1:
+    if (k->throughPlane() == 1) {
       return std::make_unique<GridBasis<3, 1>>(
         dynamic_cast<SizedKernel<3, 1> const *>(k), m, b, fg);
-    case 3:
+    } else {
       return std::make_unique<GridBasis<3, 3>>(
         dynamic_cast<SizedKernel<3, 3> const *>(k), m, b, fg);
     }
-    __builtin_unreachable();
+  case 4:
+    if (k->throughPlane() == 1) {
+      return std::make_unique<GridBasis<4, 1>>(
+        dynamic_cast<SizedKernel<4, 1> const *>(k), m, b, fg);
+    } else {
+      return std::make_unique<GridBasis<4, 4>>(
+        dynamic_cast<SizedKernel<4, 4> const *>(k), m, b, fg);
+    }
   case 5:
-    switch (k->inPlane()) {
-    case 1:
+    if (k->throughPlane() == 1) {
       return std::make_unique<GridBasis<5, 1>>(
         dynamic_cast<SizedKernel<5, 1> const *>(k), m, b, fg);
-    case 5:
+    } else {
       return std::make_unique<GridBasis<5, 5>>(
         dynamic_cast<SizedKernel<5, 5> const *>(k), m, b, fg);
     }
-    __builtin_unreachable();
+  case 6:
+    if (k->throughPlane() == 1) {
+      return std::make_unique<GridBasis<6, 1>>(
+        dynamic_cast<SizedKernel<6, 1> const *>(k), m, b, fg);
+    } else {
+      return std::make_unique<GridBasis<6, 6>>(
+        dynamic_cast<SizedKernel<6, 6> const *>(k), m, b, fg);
+    }
   }
-  __builtin_unreachable();
+  Log::Fail(FMT_STRING("No grids implemented for in-plane kernel width {}"), k->inPlane());
 }
