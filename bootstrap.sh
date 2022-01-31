9ad28e874df7d9bbd288811247bc1161a86208ff
Tobias Wood <tobias@spinicist.org.uk>
2022-01-31 20:30:27 +0000

    Add useage to bootstrap.h

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,25 @@
 #!/bin/bash -eu
 
-# This script will bootstrap an initial configure & build.
-# On Linux and Mac a toolchain will be used which includes -march=native and
-# ASan/UBSan support. Other platforms will use the default toolchain.
+USAGE="Usage: $0 [options]
+
+Bootstraps the riesling build system (specifies the toolchain for CMake)
+
+Options:
+  -f FILE : Use a set of flags for dependency compilation. Currently provided
+            options are avx2, abi (for pre-C++11 ABI), and native
+"
 
 git submodule update --init --recursive
 
-export FLAGS="base"
-while getopts "a" opt; do
+FLAGS="base"
+while getopts "f:h" opt; do
     case $opt in
-        a) export FLAGS="avx2";;
+        f) export FLAGS="$OPTARG";;
+        h) echo "$USAGE"
+           return;;
     esac
 done
+shift $((OPTIND - 1))
 
 # Use Ninja if available, otherwise CMake default
 if [ -x "$( command -v ninja )" ]; then
