3da29ddf926c082d5dbd443d438095fc320657b7
Tobias Wood <tobias@spinicist.org.uk>
2022-02-21 21:58:51 +0000

    Fix failing SDC test

--- test/sdc.cpp
+++ test/sdc.cpp
@@ -23,18 +23,17 @@ TEST_CASE("SDC")
   auto const points = ArchimedeanSpiral(info.read_points, info.spokes);
   Trajectory const traj(info, points);
 
-  Log::SetLevel(Log::Level::Info);
   SECTION("Pipe")
   {
     R2 sdc = SDC::Pipe(traj, false, 2.1f);
     CHECK(sdc.dimension(0) == info.read_points);
     CHECK(sdc.dimension(1) == info.spokes);
     // Central points should be very small
-    CHECK(sdc(0, 0) == Approx(0.00129f).margin(1.e-4f));
-    CHECK(sdc(1, 0) == Approx(0.00519f).margin(1.e-4f));
+    CHECK(sdc(0, 0) == Approx(0.00106f).margin(1.e-4f));
+    CHECK(sdc(1, 0) == Approx(0.00427f).margin(1.e-4f));
     // Undersampled points should be close to one
-    CHECK(sdc(25, 0) == Approx(0.99172f).margin(1.e-1f));
-    CHECK(sdc(26, 0) == Approx(1.08636f).margin(1.e-1f));
+    CHECK(sdc(25, 0) == Approx(0.81739f).margin(1.e-1f));
+    CHECK(sdc(26, 0) == Approx(0.8954f).margin(1.e-1f));
     // Point excluded by margin at edge of grid
     CHECK(sdc(31, 0) == Approx(0.0f).margin(1.e-4f));
   }
@@ -45,13 +44,11 @@ TEST_CASE("SDC")
     CHECK(sdc.dimension(0) == info.read_points);
     CHECK(sdc.dimension(1) == info.spokes);
     // Central points should be very small
-    CHECK(sdc(0, 0) == Approx(0.00129f).margin(1.e-4f));
-    CHECK(sdc(1, 0) == Approx(0.00519f).margin(1.e-4f));
-    // Undersampled points should be close to one
-    CHECK(sdc(25, 0) == Approx(0.99172f).margin(1.e-1f));
-    CHECK(sdc(26, 0) == Approx(1.08636f).margin(1.e-1f));
-    // Point excluded by margin at edge of grid
-    CHECK(sdc(31, 0) == Approx(0.0f).margin(1.e-4f));
+    CHECK(sdc(0, 0) == Approx(0.00042f).margin(1.e-4f));
+    CHECK(sdc(1, 0) == Approx(0.00568f).margin(1.e-4f));
+    // Undersampled points should be constant
+    CHECK(sdc(25, 0) == Approx(0.10798f).margin(1.e-1f));
+    CHECK(sdc(26, 0) == Approx(0.10798f).margin(1.e-1f));
+    CHECK(sdc(31, 0) == Approx(0.10798f).margin(1.e-4f));
   }
-  Log::SetLevel(Log::Level::None);
 }
