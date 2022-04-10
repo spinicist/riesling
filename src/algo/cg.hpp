6cd4cd040b7f2b5ced72751bb791970a888aab5d
Tobias Wood <tobias.wood@kcl.ac.uk>
2022-04-10 11:05:39 +0100

    Remove CG scaling

--- src/algo/cg.hpp
+++ src/algo/cg.hpp
@@ -37,14 +37,12 @@ struct NormalEqOp
 template <typename Op>
 typename Op::Input cg(
   Index const &max_its,
-  float const &thresh,
+  float const &tol,
   Op const &op,
   typename Op::Input const &b,
   typename Op::Input const &x0 = typename Op::Input(),
   bool const debug = false)
 {
-  float const scale = Norm(b);
-  Log::Print(FMT_STRING("Starting Conjugate Gradients, scale {} threshold {}"), scale, thresh);
   auto dev = Threads::GlobalDevice();
   // Allocate all memory
   using T = typename Op::Input;
@@ -55,15 +53,16 @@ typename Op::Input cg(
   if (x0.size()) {
     CheckDimsEqual(dims, x0.dimensions());
     Log::Print("Warm-start CG");
-    r.device(dev) = (b - op.A(x0)) / r.constant(scale);
-    x.device(dev) = x0 / x.constant(scale);
+    r.device(dev) = b - op.A(x0);
+    x.device(dev) = x0;
   } else {
-    r.device(dev) = b / r.constant(scale);
+    r.device(dev) = b;
     x.setZero();
   }
   p.device(dev) = r;
   float r_old = Norm2(r);
-
+  float const thresh = tol * sqrt(r_old);
+  Log::Print(FMT_STRING("CG |r| {:5.3E} threshold {:5.3E}"), tol, thresh);
   for (Index icg = 0; icg < max_its; icg++) {
     q = op.A(p);
     float const alpha = r_old / Dot(p, q).real();
@@ -79,68 +78,12 @@ typename Op::Input cg(
     float const beta = r_new / r_old;
     p.device(dev) = r + p * p.constant(beta);
     float const nr = sqrt(r_new);
-    Log::Print(FMT_STRING("CG {}: ɑ {} β {} norm resid {}"), icg, alpha, beta, nr);
+    Log::Print(FMT_STRING("CG {:02d} ɑ {:5.3E} β {:5.3E} |r| {:5.3E}"), icg, alpha, beta, nr);
     if (nr < thresh) {
       Log::Print(FMT_STRING("Reached convergence threshold"));
       break;
     }
     r_old = r_new;
   }
-  return x * x.constant(scale);
-}
-
-/*
- * Pre-conditioned Conjugate gradients
- */
-template <typename Op, typename Precond>
-typename Op::Input pcg(
-  Index const &max_its,
-  float const &thresh,
-  Op const &op,
-  typename Op::Input const &b,
-  typename Op::Input const &x0,
-  Precond const *P)
-{
-  float const scale = Norm(b);
-  Log::Print(
-    FMT_STRING("Starting Preconditioned Conjugate Gradients, scale {} threshold {}"),
-    scale,
-    thresh);
-  auto dev = Threads::GlobalDevice();
-  // Allocate all memory
-  using T = typename Op::Input;
-  CheckDimsEqual(op.outputDimensions(), b.dimensions());
-  auto const dims = op.inputDimensions();
-  CheckDimsEqual(dims, x0.dimensions());
-  T q(dims), p(dims), r(dims), Pr(dims), x(dims);
-  Log::Print("Initialising PCG with initial guess");
-  Pr.device(dev) = (b - op.A(x0)) / r.constant(scale);
-  r = P->apply(Pr);
-  x.device(dev) = x0 / x.constant(scale);
-
-  p.device(dev) = r;
-  float r_old = std::real(Dot(r, Pr));
-
-  for (Index icg = 0; icg < max_its; icg++) {
-    q = op.A(p);
-    float const alpha = r_old / Dot(p, q).real();
-    x.device(dev) = x + p * p.constant(alpha);
-    Log::Image(p, fmt::format(FMT_STRING("cg-p-{:02}"), icg));
-    Log::Image(q, fmt::format(FMT_STRING("cg-q-{:02}"), icg));
-    Log::Image(x, fmt::format(FMT_STRING("cg-x-{:02}"), icg));
-    Log::Image(r, fmt::format(FMT_STRING("cg-r-{:02}"), icg));
-    Pr.device(dev) = r - q * q.constant(alpha);
-    r = P->apply(Pr);
-    float const r_new = std::real(Dot(r, Pr));
-    float const beta = r_new / r_old;
-    p.device(dev) = r + p * p.constant(beta);
-    float const nr = sqrt(r_new);
-    Log::Print(FMT_STRING("CG {}: ɑ {} β {} norm resid {}"), icg, alpha, beta, nr);
-    if (nr < thresh) {
-      Log::Print(FMT_STRING("Reached convergence threshold"));
-      break;
-    }
-    r_old = r_new;
-  }
-  return x * x.constant(scale);
+  return x;
 }
