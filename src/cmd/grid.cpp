0a8a23ca4cbd1fd580c35e237f2d616c651ddd39
Tobias Wood <tobias.wood@kcl.ac.uk>
2022-04-28 15:51:14 +0100

    Make forward gridding the default

--- src/cmd/grid.cpp
+++ src/cmd/grid.cpp
@@ -11,7 +11,7 @@
 int main_grid(args::Subparser &parser)
 {
   COMMON_RECON_ARGS;
-  args::Flag forward(parser, "F", "Apply forward gridding (to non-cartesian)", {'f', "fwd"});
+  args::Flag adjoint(parser, "A", "Apply adjoint gridding (to cartesian)", {'a', "adj"});
   ParseCommand(parser, iname);
   HD5::RieslingReader reader(iname.Get());
   auto const traj = reader.trajectory();
@@ -24,16 +24,15 @@ int main_grid(args::Subparser &parser)
   HD5::Writer writer(OutName(iname.Get(), oname.Get(), "grid", "h5"));
   writer.writeTrajectory(traj);
   auto const start = Log::Now();
-  if (forward) {
-    rad_ks = gridder->A(reader.readTensor<Cx5>(HD5::Keys::Cartesian));
-    writer.writeTensor(
-      Cx4(rad_ks.reshape(Sz4{rad_ks.dimension(0), rad_ks.dimension(1), rad_ks.dimension(2), 1})),
-      HD5::Keys::Noncartesian);
-    Log::Print(FMT_STRING("Wrote non-cartesian k-space. Took {}"), Log::ToNow(start));
-  } else {
+  if (adjoint) {
     auto const sdc = SDC::Choose(sdcType.Get(), traj, osamp.Get(), sdcPow.Get());
     writer.writeTensor(gridder->Adj(sdc->Adj(reader.noncartesian(0))), "cartesian");
     Log::Print(FMT_STRING("Wrote cartesian k-space. Took {}"), Log::ToNow(start));
+  } else {
+    rad_ks = gridder->A(reader.readTensor<Cx5>(HD5::Keys::Cartesian));
+    writer.writeTensor(
+      Cx4(rad_ks.reshape(Sz4{rad_ks.dimension(0), rad_ks.dimension(1), rad_ks.dimension(2), 1})), HD5::Keys::Noncartesian);
+    Log::Print(FMT_STRING("Wrote non-cartesian k-space. Took {}"), Log::ToNow(start));
   }
 
   return EXIT_SUCCESS;
