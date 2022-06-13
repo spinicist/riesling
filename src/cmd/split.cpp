549b4d92316048fa250fd3c8c41f1c16a8a1c86a
Tobias Wood <tobias@spinicist.org.uk>
2022-06-13 22:32:40 +0100

    Support lo-res at end, splitting a volume

--- src/cmd/split.cpp
+++ src/cmd/split.cpp
@@ -9,7 +9,7 @@ int main_split(args::Subparser &parser)
 {
   args::Positional<std::string> iname(parser, "FILE", "HD5 file to recon");
   args::ValueFlag<std::string> oname(parser, "OUTPUT", "Override output name", {'o', "out"});
-  args::ValueFlag<Index> lores(parser, "N", "Extract first N spokes as lo-res", {'l', "lores"}, 0);
+  args::ValueFlag<Index> lores(parser, "N", "Extract N spokes as lo-res (negative at end)", {'l', "lores"}, 0);
   args::ValueFlag<Index> spoke_stride(parser, "S", "Hi-res stride", {"stride"}, 1);
   args::ValueFlag<Index> spoke_size(parser, "SZ", "Size of hi-res spokes to keep", {"size"});
   args::ValueFlag<Index> spi(parser, "SPOKES", "Spokes per interleave", {"spi"});
@@ -18,12 +18,24 @@ int main_split(args::Subparser &parser)
   args::ValueFlag<Index> step(parser, "STEP", "Step size", {"s", "step"}, 0);
   args::ValueFlag<Index> zero(parser, "Z", "Zero the first N samples", {"zero"}, 0);
   args::ValueFlag<Index> trim(parser, "T", "Trim the first N samples", {"trim"}, 0);
+  args::ValueFlag<Index> vol(parser, "V", "Take this volume", {"vol"});
 
   ParseCommand(parser, iname);
 
   HD5::RieslingReader reader(iname.Get());
   auto traj = reader.trajectory();
   Cx4 ks = reader.readTensor<Cx4>(HD5::Keys::Noncartesian);
+
+  if (vol) {
+    auto info = traj.info();
+    if (vol.Get() >= info.volumes) {
+      Log::Fail("Specified volume {} past end of file", vol.Get());
+    }
+    Log::Print("Selecting volume {}", vol.Get());
+    info.volumes = 1;
+    traj = Trajectory(info, traj.points(), traj.frames());
+    ks = Cx4(ks.slice(Sz4{0, 0, 0, vol.Get()}, Sz4{info.channels, info.read_points, info.spokes, 1}));
+  }
 
   if (trim) {
     Log::Print(FMT_STRING("Trimming {} points"), trim.Get());
@@ -40,27 +52,31 @@ int main_split(args::Subparser &parser)
     ks.slice(Sz4{0, 0, 0, 0}, Sz4{ks.dimension(0), zero.Get(), ks.dimension(2), ks.dimension(3)}).setZero();
   }
 
-  // Deal with this first
   if (lores) {
-    if ((lores.Get() < 1) || (lores.Get() > traj.info().spokes)) {
+    if (lores.Get() > traj.info().spokes) {
       Log::Fail(FMT_STRING("Invalid number of low-res spokes {}"), lores.Get());
     }
 
-    Info lo_info = traj.info();
-    lo_info.spokes = lores.Get();
-    I1 const lo_frames = traj.frames().slice(Sz1{0}, Sz1{lo_info.spokes});
-    Log::Print(FMT_STRING("Extracting spokes {}-{} as low-res"), 0, lo_info.spokes);
-
-    Trajectory lo_traj(
-      lo_info, traj.points().slice(Sz3{0, 0, 0}, Sz3{3, lo_info.read_points, lo_info.spokes}), lo_frames);
-    Cx4 lo_ks = ks.slice(Sz4{0, 0, 0, 0}, Sz4{lo_info.channels, lo_info.read_points, lo_info.spokes, lo_info.volumes});
-
     auto info = traj.info();
-    info.spokes -= lo_info.spokes;
-    I1 const hi_frames(traj.frames().slice(Sz1{lo_info.spokes}, Sz1{info.spokes}));
+    Index const spokesLo = std::abs(lores.Get());
+    Index const spokesHi = info.spokes - spokesLo;
+    bool atEnd = (lores.Get() < 0);
+    I1 const lo_frames = traj.frames().slice(Sz1{atEnd ? spokesHi : 0}, Sz1{spokesLo});
+    Log::Print(FMT_STRING("Extracting spokes {}-{} as low-res"), atEnd ? spokesHi : 0, spokesLo);
+
+    Info lo_info = traj.info();
+    lo_info.spokes = spokesLo;
+    Trajectory lo_traj(
+      lo_info, traj.points().slice(Sz3{0, 0, atEnd ? spokesHi : 0}, Sz3{3, lo_info.read_points, spokesLo}), lo_frames);
+    Cx4 lo_ks = ks.slice(
+      Sz4{0, 0, atEnd ? spokesHi : 0, 0}, Sz4{lo_info.channels, lo_info.read_points, spokesLo, lo_info.volumes});
+
+    info.spokes = spokesHi;
+    I1 const hi_frames(traj.frames().slice(Sz1{atEnd ? 0 : spokesLo}, Sz1{spokesHi}));
     traj = Trajectory(
-      info, R3(traj.points().slice(Sz3{0, 0, lo_info.spokes}, Sz3{3, info.read_points, info.spokes})), hi_frames);
-    ks = Cx4(ks.slice(Sz4{0, 0, lo_info.spokes, 0}, Sz4{info.channels, info.read_points, info.spokes, info.volumes}));
+      info, R3(traj.points().slice(Sz3{0, 0, atEnd ? 0 : spokesLo}, Sz3{3, info.read_points, spokesHi})), hi_frames);
+    ks =
+      Cx4(ks.slice(Sz4{0, 0, atEnd ? 0 : spokesLo, 0}, Sz4{info.channels, info.read_points, spokesHi, info.volumes}));
 
     HD5::Writer writer(OutName(iname.Get(), oname.Get(), "lores"));
     writer.writeTrajectory(lo_traj);
