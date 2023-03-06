2e940b552ea85f2c01ce207ebe883f43ce2c662a
Tobias Wood <tobias@spinicist.org.uk>
2023-03-06 11:20:36 +0000

    Rewrite entropy to excise boost

--- src/prox/entropy.cpp
+++ src/prox/entropy.cpp
@@ -3,9 +3,6 @@
 #include "log.hpp"
 #include "tensorOps.hpp"
 
-// #define BOOST_MATH_OVERFLOW_ERROR_POLICY ignore_error
-#include <boost/math/special_functions/lambert_w.hpp>
-
 namespace rl {
 
 Entropy::Entropy(float const λ)
@@ -15,14 +12,16 @@ Entropy::Entropy(float const λ)
   Log::Print(FMT_STRING("MaxEnt Prox λ {}"), λ);
 }
 
-auto Entropy::operator()(float const α, Eigen::TensorMap<Cx4 const> x) const -> Cx4
+auto Entropy::operator()(float const α, Eigen::TensorMap<Cx4 const> v) const -> Cx4
 {
-  double const t = α * λ_;
-  Re4 const xabs = x.abs();
-  Re4 const px = xabs.unaryExpr([t](float z) { return t * boost::math::lambert_w0(exp(static_cast<double>(z) / t - 1.) / t); })
-                   .cast<float>();
-  Cx4 const s = x * (px / xabs).cast<Cx>();
-  Log::Print<Log::Level::High>(FMT_STRING("Prox Entropy α {} λ {} t {} |x| {} |s| {}"), α, λ_, t, Norm(x), Norm(s));
+  float const t = α * λ_;
+  Re4 const vabs = v.abs();
+  Re4 x = vabs;
+  for (int ii = 0; ii < 8; ii++) {
+      x.device(Threads::GlobalDevice()) = (x - (t/2.f) * (x.log() + 1.f)).cwiseMax(0.f);
+  }
+  Cx4 const s = v * (x / vabs).cast<Cx>();
+  Log::Print(FMT_STRING("Entropy α {} λ {} t {} |x| {} |s| {}"), α, λ_, t, Norm(x), Norm(s));
   return s;
 }
 
@@ -33,40 +32,18 @@ NMREnt::NMREnt(float const λ)
   Log::Print(FMT_STRING("NMR Entropy Prox λ {}"), λ);
 }
 
-float grad(float const z)
-{
-  return -z / sqrt(pow(z, 2) + 1) + z * (z / sqrt(pow(z, 2) + 1) + 1) / (z + sqrt(pow(z, 2) + 1)) +
-         log(z + sqrt(pow(z, 2) + 1));
-}
-
-auto NMREnt::operator()(float const α, Eigen::TensorMap<Cx4 const> x) const -> Cx4
+auto NMREnt::operator()(float const α, Eigen::TensorMap<Cx4 const> v) const -> Cx4
 {
   float const t = α * λ_;
-
-  auto iter_prox = [t](float const v) {
-    float x = v;
-    float l = 0.f, u = std::numeric_limits<float>::infinity();
-
-    for (int ii = 0; ii < 8; ii++) {
-      float const h = grad(x);
-      float const g = h + (x - v) / t;
-      if (g > 0.f) {
-        l = x - t * g;
-        u = x;
-      } else {
-        l = x;
-        u = x - t * g;
-      }
-      x = (l + u) / 2.f;
-    }
-    return x;
-  };
-
-  Re4 const xabs = x.abs();
-  Re4 const px = xabs.unaryExpr(iter_prox);
-  Cx4 const s = x * (px / xabs).cast<Cx>();
-  Log::Print<Log::Level::High>(FMT_STRING("NMR Entropy α {} λ {} t {} |x| {} |s| {}"), α, λ_, t, Norm(x), Norm(s));
-
+  Re4 const vabs = v.abs();
+  Re4 x = vabs;
+  for (int ii = 0; ii < 8; ii++) {
+      auto const &xx = (x.square() + 1.f).sqrt();
+      x.device(Threads::GlobalDevice()) =
+        (x - (t / 2.f) * ((x * (x / xx + 1.f)) / (x + xx) + (x + xx).log() - x / xx)).cwiseMax(0.f);
+  }
+  Cx4 const s = v * (x / vabs).cast<Cx>();
+  Log::Print(FMT_STRING("NMR Entropy α {} λ {} t {} |x| {} |s| {}"), α, λ_, t, Norm(x), Norm(s));
   return s;
 }
 
--- src/prox/thresh.cpp
+++ src/prox/thresh.cpp
@@ -18,7 +18,7 @@ auto SoftThreshold<T>::operator()(float const α, Eigen::TensorMap<T const> x) c
 {
   float t = α * λ_;
   T s = (x.abs() > t).select(x * (x.abs() - t) / x.abs(), x.constant(0.f));
-  Log::Print<Log::Level::High>(FMT_STRING("Soft Threshold α {} λ {} t {} |x| {} |s| {}"), α, λ_, t, Norm(x), Norm(s));
+  Log::Print(FMT_STRING("Soft Threshold α {} λ {} t {} |x| {} |s| {}"), α, λ_, t, Norm(x), Norm(s));
   return s;
 }
 
