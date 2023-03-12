418b1135863ba2e55d05bd049c2a480113b64955
Martin Krämer <MartinKraemer84@gmail.com>
2023-03-12 16:00:03 +0100

    Added riesling_read() function for generic loading of riesling data as an xarray object

--- python/riesling/data.py
+++ python/riesling/data.py
@@ -1,4 +1,5 @@
 import h5py
+import xarray as xr
 import numpy as np
 
 INFO_STRUCTURE = ['matrix', 'tr', 'voxel_size', 'origin', 'direction']
@@ -15,6 +16,49 @@ def _read_info(hdf5_dataset):
     for key, item in zip(INFO_STRUCTURE, info):
         d[key] = item
     return d
+
+def riesling_read(filename):
+    with h5py.File(filename) as f:
+        # load meta data, everything that is not the actualy data (e.g. kspace/image) is 
+        # considered meta data including trajectory and sampling density correction factors
+        meta = {}
+        if 'info' in f.keys():
+            info = _read_info(f['info'])
+            meta.update(info)
+        if 'trajectory' in f.keys():
+            meta['trajectory'] = np.array(f['trajectory'])
+            
+        # try to load actual data
+        data = None
+        dims = []
+        data_keys = np.array(['noncartesian','cartesian','channels','image','sense'])
+        data_idx = np.in1d(data_keys, np.array([str(f) for f in f.keys()]))
+        if data_idx.any():
+            key = data_keys[data_idx][0]
+            data = f[key]
+            if key == 'noncartesian':
+                dims = ['channel', 'sample', 'trace', 'slab', 'volume']
+            elif key == 'cartesian' or key == 'channels':
+                dims = ['channel', 'image', 'x', 'y', 'z']
+            elif key == 'sense':
+                dims = ['channel', 'x', 'y', 'z']
+            elif key == 'image':
+                dims = ['image', 'x', 'y', 'z', 'volume']
+            else: # this should never happen
+                return None 
+            dims.reverse() # invert dimension order to match numpy array shape
+        else:
+            print(f'Could not find any keys matching {data_keys} in file')
+            return None
+        
+        # build xarray output
+        if (data.ndim) != len(dims):
+            print(f'Number of dimensions in data does not match expected number of dimensions')
+            return None
+        data = xr.DataArray(data, dims = dims)
+        data.attrs.update(meta)
+        
+        return data
 
 def write_noncartesian(fname, kspace, traj, matrix, voxel_size=[1,1,1], tr=1, origin=[0,0,0], direction=np.eye(3)):
     if kspace.ndim != 5:
