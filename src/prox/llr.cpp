b4650d553d935f682472b3507bb8d25271af95e2
Tobias Wood <tobias@spinicist.org.uk>
2023-05-31 21:22:30 +0100

    Improve edges for LLR

--- src/prox/llr.cpp
+++ src/prox/llr.cpp
@@ -1,9 +1,9 @@
 #include "llr.hpp"
 
 #include "algo/decomp.hpp"
+#include "log.hpp"
 #include "tensorOps.hpp"
 #include "threads.hpp"
-#include "log.hpp"
 
 #include <cmath>
 #include <random>
@@ -39,27 +39,39 @@ void LLR::apply(float const α, CMap const &xin, Map &zin) const
   std::mt19937 gen(rd());
   for (Index ii = 0; ii < 3; ii++) {
     auto const d = x.dimension(ii + 1);
-    nWindows[ii] = ((d - 1) / windowSize) - 1;
-    std::uniform_int_distribution<> int_dist(0, d - nWindows[ii] * windowSize);
+    nWindows[ii] = (d / windowSize) + 2;
+    std::uniform_int_distribution<> int_dist(0, windowSize - 1);
     shift[ii] = int_dist(gen);
   }
+  Log::Print<Log::Level::Debug>("LLR Shift {}", shift);
   Index const K = x.dimension(0);
   Sz4 const szP{K, patchSize, patchSize, patchSize};
-  Sz4 const szW{K, windowSize, windowSize, windowSize};
   Index const inset = (patchSize - windowSize) / 2;
   Eigen::TensorMap<Cx4> z(zin.data(), shape);
-  z.setZero();
-  float const realλ = λ * α;
+  z.setConstant(25.f);
+  float const realλ = λ * α * std::sqrt(patchSize*patchSize*patchSize);
   auto zTask = [&](Index const iz) {
     for (Index iy = 0; iy < nWindows[1]; iy++) {
       for (Index ix = 0; ix < nWindows[0]; ix++) {
-        Sz3 ind{ix, iy, iz};
-        Sz4 stP, stW, stW2;
+        Sz3 ind{ix - 1, iy - 1, iz - 1};
+        Sz4 stP, stW, stW2, szW;
         stP[0] = stW[0] = stW2[0] = 0;
+        szW[0] = K;
+        bool empty = false;
         for (Index ii = 0; ii < 3; ii++) {
-          stW[ii + 1] = ind[ii] * windowSize + shift[ii];
-          stP[ii + 1] = std::clamp(stW[ii + 1] - inset, 0L, x.dimension(ii + 1) - patchSize);
+          Index const d = x.dimension(ii + 1);
+          Index const st = ind[ii] * windowSize + shift[ii];
+          stW[ii + 1] = std::max(st, 0L);
+          szW[ii + 1] = windowSize + std::min({st, 0L, d - stW[ii + 1] - windowSize});
+          if (szW[ii + 1] == 0) {
+            empty = true;
+            break;
+          }
+          stP[ii + 1] = std::clamp(st - inset, 0L, d - patchSize);
           stW2[ii + 1] = stW[ii + 1] - stP[ii + 1];
+        }
+        if (empty) {
+          continue;
         }
         Cx4 patchTensor = x.slice(stP, szP);
         Eigen::MatrixXcf patch = CollapseToMatrix(patchTensor);
