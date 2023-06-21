3b2800b0d161330672508db18514286817b01763
Tobias Wood <tobias@spinicist.org.uk>
2023-06-21 17:54:38 +0100

    Add intensity offset to avoid SENSE voids

--- src/phantom/gradcubes.cpp
+++ src/phantom/gradcubes.cpp
@@ -28,11 +28,11 @@ Cx3 GradCubes(Sz3 const &matrix, Eigen::Array3f const &voxel_size, float const h
         Eigen::Array3f const p{px, py, pz};
         Eigen::Array3f const r = p / hsz; // Normalize coordinates between -1 and 1
         if ((r.abs() <= 1.f/3.f).all()) {
-            phan(ix, iy, iz) = r(0)*3.f + 1.f;
+            phan(ix, iy, iz) = r(0)*3.f + 2.f;
         } else if ((r.abs() <= 2.f/3.f).all()) {
-            phan(ix, iy, iz) = r(1)*3.f/2.f + 1.f;
+            phan(ix, iy, iz) = r(1)*3.f/2.f + 2.f;
         } else if ((r.abs() <= 1.f).all()) {
-            phan(ix, iy, iz) = r(2) + 1.f;
+            phan(ix, iy, iz) = r(2) + 2.f;
         }
       }
     }
