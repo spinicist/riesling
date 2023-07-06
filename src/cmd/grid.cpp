777954f8679b8a84c290065e813b7a87d8aa84a3
Tobias Wood <tobias@spinicist.org.uk>
2023-07-06 13:54:30 +0100

    Remove dead option

--- src/cmd/grid.cpp
+++ src/cmd/grid.cpp
@@ -15,7 +15,6 @@ int main_grid(args::Subparser &parser)
   CoreOpts coreOpts(parser);
   SDC::Opts sdcOpts(parser, "pipe");
   args::Flag fwd(parser, "", "Apply forward operation", {'f', "fwd"});
-  args::Flag bucket(parser, "", "Use bucket gridder", {"bucket"});
 
   ParseCommand(parser, coreOpts.iname);
   HD5::Reader reader(coreOpts.iname.Get());
