35a469d5b187b5c3d67d0be8075c8bc57976fee3
Tobias Wood <tobias@spinicist.org.uk>
2023-07-12 20:58:48 +0100

    SENSE precond needs bias term

--- src/precond.hpp
+++ src/precond.hpp
@@ -9,7 +9,7 @@ namespace rl {
 auto KSpaceSingle(Trajectory const &traj, Re2 const &basis, float const bias = 1.f) -> Re2;
 
 auto make_kspace_pre(
-  std::string const &type, Index const nC, Trajectory const &traj, Re2 const &basis, float const bias)
+  std::string const &type, Index const nC, Trajectory const &traj, Re2 const &basis, float const bias = 1.f)
   -> std::shared_ptr<Ops::Op<Cx>>;
 
 } // namespace rl
