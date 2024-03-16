26df0930a6b43339ed335434a711a9825814e1de
Tobias Wood <tobias@spinicist.org.uk>
2024-03-16 23:19:18 +0000

    Add compressor checks

--- src/compressor.cpp
+++ src/compressor.cpp
@@ -10,6 +10,9 @@ Cx4 Compressor::compress(Cx4 const &source)
 {
   Log::Print("Compressing to {} channels", psi.cols());
   auto const sourcemat = CollapseToConstMatrix(source);
+  if (sourcemat.rows() != psi.rows()) {
+    Log::Fail("Number of channels in data {} does not match compression matrix {}", sourcemat.rows(), psi.rows());
+  }
   Cx4        dest(psi.cols(), source.dimension(1), source.dimension(2), source.dimension(3));
   auto       destmat = CollapseToMatrix(dest);
   destmat.noalias() = psi.transpose() * sourcemat;
@@ -20,6 +23,9 @@ Cx5 Compressor::compress(Cx5 const &source)
 {
   Log::Print("Compressing to {} channels", psi.cols());
   auto const sourcemat = CollapseToConstMatrix(source);
+  if (sourcemat.rows() != psi.rows()) {
+    Log::Fail("Number of channels in data {} does not match compression matrix {}", sourcemat.rows(), psi.rows());
+  }
   Cx5        dest(psi.cols(), source.dimension(1), source.dimension(2), source.dimension(3), source.dimension(4));
   auto       destmat = CollapseToMatrix(dest);
   destmat.noalias() = psi.transpose() * sourcemat;
