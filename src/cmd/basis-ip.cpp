8971057226c20e1ca2b7cd2800187634a46f70e4
Tobias Wood <tobias@spinicist.org.uk>
2024-03-19 15:44:29 +0000

    Add gap to IP basis

--- src/cmd/basis-ip.cpp
+++ src/cmd/basis-ip.cpp
@@ -17,19 +17,23 @@ int main_basis_ip(args::Subparser &parser)
   args::Positional<std::string> oname(parser, "OUTPUT", "Name for the basis file");
 
   args::ValueFlag<Index> samples(parser, "S", "Number of samples (1)", {"samples", 's'}, 1);
+  args::ValueFlag<Index> gap(parser, "G", "Gap before samples begin", {"gap", 'g'}, 0);
   args::ValueFlag<Index> tSamp(parser, "T", "Sample time (10μs)", {"tsamp", 't'}, 10);
   args::ValueFlag<Index> freq(parser, "F", "Fat frequency (-450 Hz)", {"freq", 'f'}, -450.f);
   args::Flag             fw(parser, "W", "Fat-Water not IP/OP", {"fw"});
   ParseCommand(parser, oname);
 
   auto const nS = samples.Get();
-  auto const maxPhase = nS * tSamp.Get() * 1e-6f * freq.Get() * 2.f * std::numbers::pi_v<float>;
+  auto const nG = gap.Get();
+  auto const sampPhase = tSamp.Get() * 1e-6f * freq.Get() * 2.f * std::numbers::pi_v<float>;
+  auto const startPhase = nG * sampPhase; 
+  auto const endPhase = (nG + nS - 1) * sampPhase;
 
-  rl::Log::Print("Samples {} Max accumulated phase {} radians", nS, maxPhase);
+  rl::Log::Print("Gap {} Samples {} Max accumulated phase {} radians", nG, nS, endPhase);
 
   Eigen::VectorXcf water = Eigen::VectorXcf::Ones(nS);
 
-  Eigen::VectorXcf const ph = Eigen::VectorXcf::LinSpaced(nS, 0.f, maxPhase * 1if);
+  Eigen::VectorXcf const ph = Eigen::VectorXcf::LinSpaced(nS, startPhase * 1if, endPhase * 1if);
   Eigen::VectorXcf const fat = ph.array().exp();
 
   Eigen::MatrixXcf basis = Eigen::MatrixXcf::Zero(2, nS);
