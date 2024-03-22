db2a8f2d6f083d00407a050657461cee624c5a61
Tobias Wood <tobias@spinicist.org.uk>
2024-03-22 22:20:23 +0000

    Don't let ephemeral mode overwrite prompt

--- src/log.cpp
+++ src/log.cpp
@@ -38,6 +38,8 @@ void SetLevel(Level const l)
   } else {
     isTTY = false;
   }
+  // Move the cursor one more line down so we don't erase command names etc.
+  if (CurrentLevel() == Level::Ephemeral) { fmt::print(stderr, "\n"); }
 }
 
 void SetDebugFile(std::string const &fname) { debug_file = std::make_shared<HD5::Writer>(fname); }
