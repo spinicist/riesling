251553ebc9285c269138c2ae25be45776d23dd1c
Tobias Wood <tobias@spinicist.org.uk>
2024-04-19 11:05:10 +0100

    Remove constant term

--- cxx/vineyard/basis/bernstein.cpp
+++ cxx/vineyard/basis/bernstein.cpp
@@ -19,12 +19,11 @@ auto BernsteinPolynomial(Index const N, Index const traces) -> Basis<Cx>
 {
   Eigen::ArrayXf const x = Eigen::ArrayXf::LinSpaced(traces, 0.f, 1.f);
 
-  Basis<Cx> basis(N + 2, 1, traces);
-  basis.setConstant(1.f);
+  Basis<Cx> basis(N + 1, 1, traces);
   for (Index ii = 0; ii <= N; ii++) {
     Eigen::ArrayXf const b = Choose(N, ii) * x.pow(ii) * (1.f - x).pow(N - ii);
     for (Index it = 0; it < traces; it++) {
-      basis(ii + 1, 0, it) = b(it);
+      basis(ii, 0, it) = b(it);
     }
   }
   return basis;
