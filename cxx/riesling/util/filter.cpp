230736b5613544c775100aa5b1e9f7534636a703
Tobias Wood <tobias@spinicist.org.uk>
2024-07-12 15:25:11 +0100

    Fix filter

--- cxx/riesling/util/filter.cpp
+++ cxx/riesling/util/filter.cpp
@@ -1,9 +1,9 @@
 #include "types.hpp"
 
-#include "op/fft.hpp"
 #include "filter.hpp"
 #include "io/hd5.hpp"
 #include "log.hpp"
+#include "op/fft.hpp"
 #include "parse_args.hpp"
 #include "threads.hpp"
 
@@ -16,7 +16,6 @@ void main_filter(args::Subparser &parser)
   args::ValueFlag<float>        start(parser, "S", "Filter start in fractional k-space (default 0.5)", {"start"}, 0.5f);
   args::ValueFlag<float>        end(parser, "E", "Filter end in fractional k-space (default 1.0)", {"end"}, 1.f);
   args::ValueFlag<float>        height(parser, "H", "Filter end height (default 0.5)", {"height"}, 0.5f);
-  args::Flag                    sharpen(parser, "S", "Apply Sharpening filter", {'s', "sharpen"});
   ParseCommand(parser);
 
   if (!iname) { throw args::Error("No input file specified"); }
@@ -25,26 +24,9 @@ void main_filter(args::Subparser &parser)
   auto const  fft = TOps::FFT<4, 3>(FirstN<4>(images.dimensions()));
   for (Index iv = 0; iv < images.dimension(4); iv++) {
     Cx4 img = images.chip<4>(iv);
-    fft.forward(img);
-    if (sharpen) {
-      Cx4 filter(img.dimensions());
-      filter.setZero();
-      auto const shape = LastN<3>(img.dimensions());
-      auto const c = Div(shape, 2);
-      filter.chip<3>(c[2]).chip<2>(c[1]).chip<1>(c[0]).setConstant(7.f);
-      filter.chip<3>(c[2]).chip<2>(c[1]).chip<1>(c[0] - 1).setConstant(-1.f);
-      filter.chip<3>(c[2]).chip<2>(c[1]).chip<1>(c[0] + 1).setConstant(-1.f);
-      filter.chip<3>(c[2]).chip<2>(c[1] - 1).chip<1>(c[0]).setConstant(-1.f);
-      filter.chip<3>(c[2]).chip<2>(c[1] + 1).chip<1>(c[0]).setConstant(-1.f);
-      filter.chip<3>(c[2] - 1).chip<2>(c[1]).chip<1>(c[0]).setConstant(-1.f);
-      filter.chip<3>(c[2] + 1).chip<2>(c[1]).chip<1>(c[0]).setConstant(-1.f);
-      filter *= filter.constant(std::sqrt(Product(shape)));
-      fft.forward(filter);
-      img *= filter;
-    } else {
-      CartesianTukey(start.Get(), end.Get(), height.Get(), img);
-    }
-    fft.adjoint(img);
+    Cx4 ks = fft.forward(img);
+    CartesianTukey(start.Get(), end.Get(), height.Get(), ks);
+    img = fft.adjoint(ks);
     images.chip<4>(iv) = img;
   }
   HD5::Writer writer(oname.Get());
