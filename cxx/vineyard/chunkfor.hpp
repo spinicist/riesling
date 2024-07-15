5adf13e6ed535b6da0069f1de461d0c0681f3ab1
Tobias Wood <tobias@spinicist.org.uk>
2024-07-15 16:23:19 +0100

    ChunkFor compiles

--- cxx/vineyard/chunkfor.hpp
+++ cxx/vineyard/chunkfor.hpp
@@ -3,19 +3,25 @@
 #include "threads.hpp"
 
 #include <ranges>
+#include <tl/chunk.hpp>
 
 namespace rl::Threads {
 template <typename T, typename F> void ChunkFor(F f, std::vector<T> const &v)
 {
   Index const    nt = GlobalThreadCount();
   Eigen::Barrier barrier(static_cast<unsigned int>(nt));
-  auto const     chunks = v | std::views::chunk(nt);
+  auto const     chunks = v | tl::views::chunk(nt);
   for (auto const &chunk : chunks) {
-    GlobalPool()->Schedule([&barrier, &f, &chunk] {
+    fmt::print(stderr, "All: {} Chunk: {}\n", v.size(), chunk.size());
+    GlobalPool()->Schedule([&barrier, &f, chunk] {
       f(chunk);
+      fmt::print(stderr, "notifying\n");
       barrier.Notify();
     });
+    fmt::print(stderr, "Scheduled a thread\n");
   }
+  fmt::print(stderr, "WAITING\n");
   barrier.Wait();
+  fmt::print(stderr, "Barrier said GO\n");
 }
 } // namespace rl::Threads
