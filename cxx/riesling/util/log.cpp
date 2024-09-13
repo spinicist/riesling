dc431291487a19e37efc37667a892b40cb13dfe5
Tobias Wood <tobias@spinicist.org.uk>
2024-09-13 23:07:28 +0100

    Basic log filtering

--- cxx/riesling/util/log.cpp
+++ cxx/riesling/util/log.cpp
@@ -7,15 +7,17 @@ using namespace rl;
 void main_log(args::Subparser &parser)
 {
   args::Positional<std::string> iname(parser, "FILE", "HD5 file to dump log from");
+  args::ValueFlag<std::string>  filter(parser, "F", "Filter category", {"filter", 'f'});
   ParseCommand(parser, iname);
   auto const  cmd = parser.GetCommand().Name();
   HD5::Reader reader(iname.Get());
   if (reader.exists("log")) {
     auto const entries = reader.readStrings("log");
     for (auto const &entry : entries) {
-      fmt::print("{}\n", entry);
+      if (!filter || (entry.substr(12, 6).find(filter.Get())) != std::string::npos) { fmt::print("{}\n", entry); }
     }
   } else {
     Log::Fail(cmd, "File {} does not contain a log", iname.Get());
   }
+  Log::Print(cmd, "Finished");
 }
