64f866a1f5404783ff87fb263d622506de1af582
Tobias Wood <tobias@spinicist.org.uk>
2024-09-14 22:52:32 +0100

    Add 6D filtering

--- cxx/riesling/util/filter.cpp
+++ cxx/riesling/util/filter.cpp
@@ -20,17 +20,35 @@ void main_filter(args::Subparser &parser)
   auto const cmd = parser.GetCommand().Name();
   if (!iname) { throw args::Error("No input file specified"); }
   HD5::Reader input(iname.Get());
-  Cx5         images = input.readTensor<Cx5>();
-  auto const  fft = TOps::FFT<4, 3>(FirstN<4>(images.dimensions()));
-  for (Index iv = 0; iv < images.dimension(4); iv++) {
-    Cx4 img = images.chip<4>(iv);
-    Cx4 ks = fft.forward(img);
-    CartesianTukey(start.Get(), end.Get(), height.Get(), ks);
-    img = fft.adjoint(ks);
-    images.chip<4>(iv) = img;
+
+  if (input.order() == 5) {
+    Cx5        images = input.readTensor<Cx5>();
+    auto const fft = TOps::FFT<4, 3>(FirstN<4>(images.dimensions()));
+    for (Index iv = 0; iv < images.dimension(4); iv++) {
+      Cx4 img = images.chip<4>(iv);
+      Cx4 ks = fft.forward(img);
+      CartesianTukey(start.Get(), end.Get(), height.Get(), ks);
+      images.chip<4>(iv) = fft.adjoint(ks);
+    }
+    HD5::Writer writer(oname.Get());
+    writer.writeInfo(input.readInfo());
+    writer.writeTensor(HD5::Keys::Data, images.dimensions(), images.data(), HD5::Dims::Image);
+  } else if (input.order() == 6) {
+    Cx6        channels = input.readTensor<Cx6>();
+    auto const fft = TOps::FFT<4, 3>(MidN<1, 4>(channels.dimensions()));
+    for (Index iv = 0; iv < channels.dimension(5); iv++) {
+      for (Index ic = 0; ic < channels.dimension(0); ic++) {
+        Cx4 img = channels.chip<5>(iv).chip<0>(ic);
+        Cx4 ks = fft.forward(img);
+        CartesianTukey(start.Get(), end.Get(), height.Get(), ks);
+        channels.chip<5>(iv).chip<0>(ic) = fft.adjoint(ks);
+      }
+    }
+    HD5::Writer writer(oname.Get());
+    writer.writeInfo(input.readInfo());
+    writer.writeTensor(HD5::Keys::Data, channels.dimensions(), channels.data(), HD5::Dims::Channels);
+  } else {
+    throw Log::Failure(cmd, "Data was not order 5 or 6");
   }
-  HD5::Writer writer(oname.Get());
-  writer.writeInfo(input.readInfo());
-  writer.writeTensor(HD5::Keys::Data, images.dimensions(), images.data(), HD5::Dims::Image);
   Log::Print(cmd, "Finished");
 }
