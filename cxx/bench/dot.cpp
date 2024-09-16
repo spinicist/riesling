cb58767c48998bb5c8a6a2ec7fcb7a46dd55282b
Tobias Wood <tobias@spinicist.org.uk>
2024-09-16 12:14:37 +0100

    More benchmarks

--- cxx/bench/dot.cpp
+++ cxx/bench/dot.cpp
@@ -2,26 +2,49 @@
 
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/benchmark/catch_benchmark_all.hpp>
-#include <unsupported/Eigen/CXX11/Tensor>
+#include "sys/threads.hpp"
 
-TEST_CASE("Dot")
+TEST_CASE("Tensor Dot", "[Dot]")
 {
   using Cx4 = Eigen::Tensor<std::complex<float>, 4>;
   int const sz = 256;
   Cx4 grid(sz, sz, sz, 16);
   grid.setRandom();
-  Eigen::ThreadPool gp(std::thread::hardware_concurrency());
-  Eigen::ThreadPoolDevice dev(&gp, gp.NumThreads());
 
+  Eigen::TensorFixedSize<std::complex<float>, Eigen::Sizes<>> d;
   BENCHMARK("Naive")
   {
-    Eigen::TensorFixedSize<std::complex<float>, Eigen::Sizes<>> d;
     d = (grid.conjugate() * grid).sum();
   };
 
   BENCHMARK("Threaded")
   {
-    Eigen::TensorFixedSize<std::complex<float>, Eigen::Sizes<>> d;
-    d.device(dev) = (grid.conjugate() * grid).sum();
+    d.device(rl::Threads::TensorDevice()) = (grid.conjugate() * grid).sum();
+  };
+}
+
+TEST_CASE("Core Dot", "[Dot]")
+{
+  int const sz = 256*256*256*16;
+  Eigen::VectorXcf vec1(sz), vec2(sz); 
+  vec1.setRandom();
+  vec2.setRandom();
+
+  rl::Cx d;
+  BENCHMARK("Dot")
+  {
+    d = vec1.dot(vec2);
+  };
+
+  float n;
+
+  BENCHMARK("Norm")
+  {
+    n = vec1.norm();
+  };
+
+  BENCHMARK("Stable Norm")
+  {
+    n = vec1.stableNorm();
   };
 }
