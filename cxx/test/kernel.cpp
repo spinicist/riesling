7ad126edae7d25b50126f94549bb70fa8797027e
Tobias Wood <tobias@spinicist.org.uk>
2024-09-17 17:32:10 +0100

    Fix kernel tests

--- cxx/test/kernel.cpp
+++ cxx/test/kernel.cpp
@@ -29,7 +29,6 @@ TEST_CASE(
 TEMPLATE_TEST_CASE(
   "1D-ExpSemi",
   "[kernels]",
-  (rl::Kernel<float, 1, rl::ExpSemi<2>>),
   (rl::Kernel<float, 1, rl::ExpSemi<3>>),
   (rl::Kernel<float, 1, rl::ExpSemi<5>>))
 {
@@ -40,6 +39,27 @@ TEMPLATE_TEST_CASE(
   INFO("k0 " << rl::Transpose(k0) << " β " << kernel.f.β);
   CHECK(rl::Norm(k0) == Approx(1.f).margin(1.e-9));
   CHECK(k0(0) == Approx(0.f).margin(1.e-9));
+
+  p.setConstant(0.5f);
+  auto const k1 = kernel(p);
+  INFO("k1 " << rl::Transpose(k1));
+  CHECK(k1(0) == Approx(0.f).margin(1.e-9));
+  CHECK(k1(1) == Approx(k1(TestType::PadWidth - 1)).margin(1.e-5));
+}
+
+TEMPLATE_TEST_CASE(
+  "1D-ExpSemi",
+  "[kernels]",
+  (rl::Kernel<float, 1, rl::ExpSemi<2>>),
+  (rl::Kernel<float, 1, rl::ExpSemi<4>>))
+{
+  TestType kernel(2.f);
+  typename TestType::Point p;
+  p.setConstant(0.f);
+  auto const k0 = kernel(p);
+  INFO("k0 " << rl::Transpose(k0) << " β " << kernel.f.β);
+  CHECK(rl::Norm(k0) == Approx(1.f).margin(1.e-9));
+  CHECK(k0(0) == Approx(k0(TestType::PadWidth - 1)).margin(1.e-9));
 
   p.setConstant(0.5f);
   auto const k1 = kernel(p);
