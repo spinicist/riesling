1a3ca19dd34469b851eca323c9875fb3a6ae6bbf
Tobias Wood <tobias@spinicist.org.uk>
2024-10-23 10:45:10 +0100

    Better benchmarks

--- cxx/bench/grid.cpp
+++ cxx/bench/grid.cpp
@@ -7,6 +7,7 @@
 
 #include <catch2/benchmark/catch_benchmark_all.hpp>
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
 
 using namespace rl;
 
@@ -14,14 +15,14 @@ Index const M = 64;
 Index const C = 8;
 Index const traces = M * M;
 auto const  points = ArchimedeanSpiral(M, 1.f, traces);
-Basis       basis;
 Trajectory  traj(points);
 float const os = 2.f;
 
-TEST_CASE("Grid ES4", "[grid]")
+TEST_CASE("Grid", "[grid]")
 {
+  auto ktype = GENERATE("ES2", "ES4", "ES6");
   Log::SetLevel(Log::Level::Testing);
-  auto grid = TOps::Grid<3>::Make(traj, traj.matrix(), os, "ES4", C, &basis);
+  auto grid = TOps::Grid<3>::Make(traj, traj.matrix(), os, ktype, C, nullptr);
   Cx5  c(grid->ishape);
   Cx3  nc(grid->oshape);
   c.setRandom();
@@ -30,35 +31,18 @@ TEST_CASE("Grid ES4", "[grid]")
   Cx3Map  mnc(nc.data(), nc.dimensions());
   Cx5CMap cc(c.data(), c.dimensions());
   Cx3CMap cnc(nc.data(), nc.dimensions());
-  BENCHMARK("forward") { grid->forward(cc, mnc); };
-  BENCHMARK("iforward") { grid->iforward(cc, mnc); };
-  BENCHMARK("adjoint") { grid->adjoint(cnc, mc); };
-  BENCHMARK("iadjoint") { grid->iadjoint(cnc, mc); };
+  BENCHMARK(fmt::format("Grid {} forward", ktype)) { grid->forward(cc, mnc); };
+  BENCHMARK(fmt::format("Grid {} iforward", ktype)) { grid->iforward(cc, mnc); };
+  BENCHMARK(fmt::format("Grid {} adjoint", ktype)) { grid->adjoint(cnc, mc); };
+  BENCHMARK(fmt::format("Grid {} iadjoint", ktype)) { grid->iadjoint(cnc, mc); };
 }
 
-TEST_CASE("Grid ES5", "[grid]")
+TEST_CASE("Grid-Basis", "[grid]")
 {
-  Log::SetLevel(Log::Level::Testing);
-  auto grid = TOps::Grid<3>::Make(traj, traj.matrix(), os, "ES5", C, &basis);
-  Cx5  c(grid->ishape);
-  Cx3  nc(grid->oshape);
-  c.setRandom();
-  nc.setRandom();
-  Cx5Map  mc(c.data(), c.dimensions());
-  Cx3Map  mnc(nc.data(), nc.dimensions());
-  Cx5CMap cc(c.data(), c.dimensions());
-  Cx3CMap cnc(nc.data(), nc.dimensions());
-  BENCHMARK("forward") { grid->forward(cc, mnc); };
-  BENCHMARK("iforward") { grid->iforward(cc, mnc); };
-  BENCHMARK("adjoint") { grid->adjoint(cnc, mc); };
-  BENCHMARK("iadjoint") { grid->iadjoint(cnc, mc); };
-}
-
-TEST_CASE("GridBasis ES4", "[grid]")
-{
+  auto ktype = GENERATE("ES2", "ES4", "ES6");
   Index const nB = 4;
   Basis       basis(nB, 1, 256);
-  auto        grid = TOps::Grid<3>::Make(traj, traj.matrix(), os, "ES4", C, &basis);
+  auto        grid = TOps::Grid<3>::Make(traj, traj.matrix(), os, ktype, C, &basis);
   Cx5         c(grid->ishape);
   Cx3         nc(grid->oshape);
   c.setRandom();
@@ -67,27 +51,8 @@ TEST_CASE("GridBasis ES4", "[grid]")
   Cx3Map  mnc(nc.data(), nc.dimensions());
   Cx5CMap cc(c.data(), c.dimensions());
   Cx3CMap cnc(nc.data(), nc.dimensions());
-  BENCHMARK("forward") { grid->forward(cc, mnc); };
-  BENCHMARK("iforward") { grid->iforward(cc, mnc); };
-  BENCHMARK("adjoint") { grid->adjoint(cnc, mc); };
-  BENCHMARK("iadjoint") { grid->iadjoint(cnc, mc); };
-}
-
-TEST_CASE("GridBasis ES5", "[grid]")
-{
-  Index const nB = 4;
-  Basis       basis(nB, 1, 256);
-  auto        grid = TOps::Grid<3>::Make(traj, traj.matrix(), os, "ES5", C, &basis);
-  Cx5         c(grid->ishape);
-  Cx3         nc(grid->oshape);
-  c.setRandom();
-  nc.setRandom();
-  Cx5Map  mc(c.data(), c.dimensions());
-  Cx3Map  mnc(nc.data(), nc.dimensions());
-  Cx5CMap cc(c.data(), c.dimensions());
-  Cx3CMap cnc(nc.data(), nc.dimensions());
-  BENCHMARK("forward") { grid->forward(cc, mnc); };
-  BENCHMARK("iforward") { grid->iforward(cc, mnc); };
-  BENCHMARK("adjoint") { grid->adjoint(cnc, mc); };
-  BENCHMARK("iadjoint") { grid->iadjoint(cnc, mc); };
+  BENCHMARK(fmt::format("Grid {} forward", ktype)) { grid->forward(cc, mnc); };
+  BENCHMARK(fmt::format("Grid {} iforward", ktype)) { grid->iforward(cc, mnc); };
+  BENCHMARK(fmt::format("Grid {} adjoint", ktype)) { grid->adjoint(cnc, mc); };
+  BENCHMARK(fmt::format("Grid {} iadjoint", ktype)) { grid->iadjoint(cnc, mc); };
 }
--- cxx/bench/kernel.cpp
+++ cxx/bench/kernel.cpp
@@ -9,14 +9,14 @@
 
 using namespace rl;
 
-TEST_CASE("Kernels", "[kernels]")
+TEMPLATE_TEST_CASE(
+  "Kernels", "[kernels]", (Kernel<Cx, 3, ExpSemi<2>>), (Kernel<Cx, 3, ExpSemi<4>>), (Kernel<Cx, 3, ExpSemi<6>>))
 {
   Log::SetLevel(Log::Level::Testing);
-  Kernel<Cx, 3, ExpSemi<3>> ES4(2.f);
-  Kernel<Cx, 3, ExpSemi<5>> es5(2.f);
-  auto const                p = Kernel<Cx, 3, ExpSemi<3>>::Point::Constant(0.5f);
+  TestType   k(2.f);
+  auto const p = TestType::Point::Constant(0.5f);
 
-  Index const B = GENERATE(1, 256); //, 4, 8, 32, 128, 256);
+  Index const B = GENERATE(1, 128);
   Index const C = 8;
   Cx1         b(B);
   b.setRandom();
@@ -26,9 +26,6 @@ TEST_CASE("Kernels", "[kernels]")
   Cx1Map                            ym(y.data(), Sz1{8});
   Eigen::Array<int16_t, 3, 1> const c{8, 8, 8};
 
-  BENCHMARK(fmt::format("ES4 Spread {}", B)) { ES4.spread(c, p, b, y, x); };
-  BENCHMARK(fmt::format("ES4 Gather {}", B)) { ES4.gather(c, p, b, x, ym); };
-
-  BENCHMARK(fmt::format("ES5 Spread {}", B)) { es5.spread(c, p, b, y, x); };
-  BENCHMARK(fmt::format("ES5 Gather {}", B)) { es5.gather(c, p, b, x, ym); };
+  BENCHMARK(fmt::format("ES{} Spread {}", TestType::Width, B)) { k.spread(c, p, b, y, x); };
+  BENCHMARK(fmt::format("ES{} Gather {}", TestType::Width, B)) { k.gather(c, p, b, x, ym); };
 }
