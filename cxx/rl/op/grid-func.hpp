ad6441695a9c3f80d147caf96d4db24e785a81e7
Tobias Wood <tobias@spinicist.org.uk>
2025-02-14 00:01:06 +0000

    WIP Coil as inner loop is better?

--- cxx/rl/op/grid-func.hpp
+++ cxx/rl/op/grid-func.hpp
@@ -112,7 +112,7 @@ template <int FW> struct GFunc<2, FW>
                              int32_t const                     trace,
                              KT const                         &k,
                              Cx3CMap                           y,
-                             Cx4                              &sg)
+                             Cx4                               sg)
   {
     for (Index ib = 0; ib < basis->nB(); ib++) {
       auto const b = basis->entry(ib, sample, trace);
@@ -134,7 +134,7 @@ template <int FW> struct GFunc<2, FW>
                             int32_t const                     trace,
                             KT const                         &k,
                             Cx4 const                        &sg,
-                            Cx3Map                           &y)
+                            Cx3Map                            y)
   {
     for (Index ib = 0; ib < basis->nB(); ib++) {
       auto const b = basis->entry(ib, sample, trace);
@@ -155,8 +155,8 @@ template <int FW> struct GFunc<3, FW>
 {
   using KT = FixedTensor<float, 3, FW>;
 
-  inline static void
-  Scatter(Eigen::Array<int16_t, 3, 1> const corner, int16_t const sample, int32_t const trace, KT const &k, Cx3CMap y, Cx5 &sg)
+  inline static void Scatter(
+    Eigen::Array<int16_t, 3, 1> const corner, int16_t const sample, int32_t const trace, KT const &k, Cx3CMap const y, Cx5 &sg)
   {
     for (Index ic = 0; ic < y.dimension(0); ic++) {
       for (Index iz = 0; iz < FW; iz++) {
@@ -175,13 +175,13 @@ template <int FW> struct GFunc<3, FW>
   inline static void Gather(
     Eigen::Array<int16_t, 3, 1> const corner, int16_t const sample, int32_t const trace, KT const &k, Cx5 const &sg, Cx3Map y)
   {
-    for (Index ic = 0; ic < y.dimension(0); ic++) {
-      for (Index iz = 0; iz < FW; iz++) {
-        Index const iiz = iz + corner[2] - FW / 2;
-        for (Index iy = 0; iy < FW; iy++) {
-          Index const iiy = iy + corner[1] - FW / 2;
-          for (Index ix = 0; ix < FW; ix++) {
-            Index const iix = ix + corner[0] - FW / 2;
+    for (Index iz = 0; iz < FW; iz++) {
+      Index const iiz = iz + corner[2] - FW / 2;
+      for (Index iy = 0; iy < FW; iy++) {
+        Index const iiy = iy + corner[1] - FW / 2;
+        for (Index ix = 0; ix < FW; ix++) {
+          Index const iix = ix + corner[0] - FW / 2;
+          for (Index ic = 0; ic < y.dimension(0); ic++) {
             y(ic, sample, trace) += sg(iix, iiy, iiz, ic, 0) * k(ix, iy, iz);
           }
         }
@@ -199,13 +199,13 @@ template <int FW> struct GFunc<3, FW>
   {
     for (Index ib = 0; ib < basis->nB(); ib++) {
       auto const b = basis->entry(ib, sample, trace);
-      for (Index ic = 0; ic < y.dimension(0); ic++) {
-        for (Index iz = 0; iz < FW; iz++) {
-          Index const iiz = iz + corner[2] - FW / 2;
-          for (Index iy = 0; iy < FW; iy++) {
-            Index const iiy = iy + corner[1] - FW / 2;
-            for (Index ix = 0; ix < FW; ix++) {
-              Index const iix = ix + corner[0] - FW / 2;
+      for (Index iz = 0; iz < FW; iz++) {
+        Index const iiz = iz + corner[2] - FW / 2;
+        for (Index iy = 0; iy < FW; iy++) {
+          Index const iiy = iy + corner[1] - FW / 2;
+          for (Index ix = 0; ix < FW; ix++) {
+            Index const iix = ix + corner[0] - FW / 2;
+            for (Index ic = 0; ic < y.dimension(0); ic++) {
               sg(iix, iiy, iiz, ic, ib) += y(ic, sample, trace) * k(ix, iy, iz) * b;
             }
           }
@@ -220,7 +220,7 @@ template <int FW> struct GFunc<3, FW>
                             int32_t const                     trace,
                             KT const                         &k,
                             Cx5 const                        &sg,
-                            Cx3Map                           &y)
+                            Cx3Map                            y)
   {
     for (Index ib = 0; ib < basis->nB(); ib++) {
       auto const b = basis->entry(ib, sample, trace);
--- cxx/rl/op/grid-subgrid.hpp
+++ cxx/rl/op/grid-subgrid.hpp
@@ -13,10 +13,10 @@ template <int SGSZ> struct GridToSubgrid<1, SGSZ>
 {
   inline static void FastCopy(Eigen::Array<int16_t, 1, 1> const sg, Cx3CMap const x, Cx3 &sx)
   {
-    for (Index ib = 0; ib < sx.dimension(2); ib++) {
-      for (Index ic = 0; ic < sx.dimension(1); ic++) {
-        for (Index ix = 0; ix < sx.dimension(0); ix++) {
-          Index const iix = ix + sg[0];
+    for (Index ix = 0; ix < SGSZ; ix++) {
+      Index const iix = ix + sg[0];
+      for (Index ib = 0; ib < sx.dimension(2); ib++) {
+        for (Index ic = 0; ic < sx.dimension(1); ic++) {
           sx(ix, ic, ib) = x(iix, ic, ib);
         }
       }
@@ -25,10 +25,10 @@ template <int SGSZ> struct GridToSubgrid<1, SGSZ>
 
   inline static void SlowCopy(Eigen::Array<int16_t, 1, 1> const sg, Cx3CMap const x, Cx3 &sx)
   {
-    for (Index ib = 0; ib < sx.dimension(2); ib++) {
-      for (Index ic = 0; ic < sx.dimension(1); ic++) {
-        for (Index ix = 0; ix < sx.dimension(0); ix++) {
-          Index const iix = Wrap(ix + sg[0], x.dimension(0));
+    for (Index ix = 0; ix < SGSZ; ix++) {
+      Index const iix = Wrap(ix + sg[0], x.dimension(0));
+      for (Index ib = 0; ib < sx.dimension(2); ib++) {
+        for (Index ic = 0; ic < sx.dimension(1); ic++) {
           sx(ix, ic, ib) = x(iix, ic, ib);
         }
       }
@@ -40,12 +40,12 @@ template <int SGSZ> struct GridToSubgrid<2, SGSZ>
 {
   inline static void FastCopy(Eigen::Array<int16_t, 2, 1> const sg, Cx4CMap const x, Cx4 &sx)
   {
-    for (Index ib = 0; ib < sx.dimension(3); ib++) {
-      for (Index ic = 0; ic < sx.dimension(2); ic++) {
-        for (Index iy = 0; iy < sx.dimension(1); iy++) {
-          Index const iiy = iy + sg[1];
-          for (Index ix = 0; ix < sx.dimension(0); ix++) {
-            Index const iix = ix + sg[0];
+    for (Index iy = 0; iy < SGSZ; iy++) {
+      Index const iiy = iy + sg[1];
+      for (Index ix = 0; ix < SGSZ; ix++) {
+        Index const iix = ix + sg[0];
+        for (Index ib = 0; ib < sx.dimension(3); ib++) {
+          for (Index ic = 0; ic < sx.dimension(2); ic++) {
             sx(ix, iy, ic, ib) = x(iix, iiy, ic, ib);
           }
         }
@@ -55,12 +55,12 @@ template <int SGSZ> struct GridToSubgrid<2, SGSZ>
 
   inline static void SlowCopy(Eigen::Array<int16_t, 2, 1> const sg, Cx4CMap const x, Cx4 &sx)
   {
-    for (Index ib = 0; ib < sx.dimension(3); ib++) {
-      for (Index ic = 0; ic < sx.dimension(2); ic++) {
-        for (Index iy = 0; iy < sx.dimension(1); iy++) {
-          Index const iiy = Wrap(iy + sg[1], x.dimension(1));
-          for (Index ix = 0; ix < sx.dimension(0); ix++) {
-            Index const iix = Wrap(ix + sg[0], x.dimension(0));
+    for (Index iy = 0; iy < SGSZ; iy++) {
+      Index const iiy = Wrap(iy + sg[1], x.dimension(1));
+      for (Index ix = 0; ix < SGSZ; ix++) {
+        Index const iix = Wrap(ix + sg[0], x.dimension(0));
+        for (Index ib = 0; ib < sx.dimension(3); ib++) {
+          for (Index ic = 0; ic < sx.dimension(2); ic++) {
             sx(ix, iy, ic, ib) = x(iix, iiy, ic, ib);
           }
         }
@@ -73,14 +73,14 @@ template <int SGSZ> struct GridToSubgrid<3, SGSZ>
 {
   inline static void FastCopy(Eigen::Array<int16_t, 3, 1> const sg, Cx5CMap const x, Cx5 &sx)
   {
-    for (Index ib = 0; ib < sx.dimension(4); ib++) {
-      for (Index ic = 0; ic < sx.dimension(3); ic++) {
-        for (Index iz = 0; iz < sx.dimension(2); iz++) {
-          Index const iiz = iz + sg[2];
-          for (Index iy = 0; iy < sx.dimension(1); iy++) {
-            Index const iiy = iy + sg[1];
-            for (Index ix = 0; ix < sx.dimension(0); ix++) {
-              Index const iix = ix + sg[0];
+    for (Index iz = 0; iz < SGSZ; iz++) {
+      Index const iiz = iz + sg[2];
+      for (Index iy = 0; iy < SGSZ; iy++) {
+        Index const iiy = iy + sg[1];
+        for (Index ix = 0; ix < SGSZ; ix++) {
+          Index const iix = ix + sg[0];
+          for (Index ib = 0; ib < sx.dimension(4); ib++) {
+            for (Index ic = 0; ic < sx.dimension(3); ic++) {
               sx(ix, iy, iz, ic, ib) = x(iix, iiy, iiz, ic, ib);
             }
           }
@@ -91,14 +91,15 @@ template <int SGSZ> struct GridToSubgrid<3, SGSZ>
 
   inline static void SlowCopy(Eigen::Array<int16_t, 3, 1> const sg, Cx5CMap const x, Cx5 &sx)
   {
-    for (Index ib = 0; ib < sx.dimension(4); ib++) {
-      for (Index ic = 0; ic < sx.dimension(3); ic++) {
-        for (Index iz = 0; iz < sx.dimension(2); iz++) {
-          Index const iiz = Wrap(iz + sg[2], x.dimension(2));
-          for (Index iy = 0; iy < sx.dimension(1); iy++) {
-            Index const iiy = Wrap(iy + sg[1], x.dimension(1));
-            for (Index ix = 0; ix < sx.dimension(0); ix++) {
-              Index const iix = Wrap(ix + sg[0], x.dimension(0));
+
+    for (Index iz = 0; iz < SGSZ; iz++) {
+      Index const iiz = Wrap(iz + sg[2], x.dimension(2));
+      for (Index iy = 0; iy < SGSZ; iy++) {
+        Index const iiy = Wrap(iy + sg[1], x.dimension(1));
+        for (Index ix = 0; ix < SGSZ; ix++) {
+          Index const iix = Wrap(ix + sg[0], x.dimension(0));
+          for (Index ib = 0; ib < sx.dimension(4); ib++) {
+            for (Index ic = 0; ic < sx.dimension(3); ic++) {
               sx(ix, iy, iz, ic, ib) = x(iix, iiy, iiz, ic, ib);
             }
           }
@@ -117,11 +118,11 @@ template <int SGSZ> struct SubgridToGrid<1, SGSZ>
   inline static void FastCopy(std::vector<std::mutex> &m, Eigen::Array<int16_t, 1, 1> const corner, Cx3CMap const sx, Cx3Map x)
   {
     assert(m.size() == x.dimension(0));
-    for (Index ib = 0; ib < sx.dimension(2); ib++) {
-      for (Index ic = 0; ic < sx.dimension(1); ic++) {
-        for (Index ix = 0; ix < SGSZ; ix++) {
-          Index const      iix = ix + corner[0];
-          std::scoped_lock lock(m[iix]);
+    for (Index ix = 0; ix < SGSZ; ix++) {
+      Index const      iix = ix + corner[0];
+      std::scoped_lock lock(m[iix]);
+      for (Index ib = 0; ib < sx.dimension(2); ib++) {
+        for (Index ic = 0; ic < sx.dimension(1); ic++) {
           x(iix, ic, ib) += sx(ix, ic, ib);
         }
       }
@@ -131,11 +132,11 @@ template <int SGSZ> struct SubgridToGrid<1, SGSZ>
   inline static void SlowCopy(std::vector<std::mutex> &m, Eigen::Array<int16_t, 1, 1> const corner, Cx3CMap const sx, Cx3Map x)
   {
     assert(m.size() == x.dimension(0));
-    for (Index ib = 0; ib < sx.dimension(2); ib++) {
-      for (Index ic = 0; ic < sx.dimension(1); ic++) {
-        for (Index ix = 0; ix < SGSZ; ix++) {
-          Index const      iix = Wrap(ix + corner[0], x.dimension(0));
-          std::scoped_lock lock(m[iix]);
+    for (Index ix = 0; ix < SGSZ; ix++) {
+      Index const      iix = Wrap(ix + corner[0], x.dimension(0));
+      std::scoped_lock lock(m[iix]);
+      for (Index ib = 0; ib < sx.dimension(2); ib++) {
+        for (Index ic = 0; ic < sx.dimension(1); ic++) {
           x(iix, ic, ib) += sx(ix, ic, ib);
         }
       }
@@ -148,13 +149,13 @@ template <int SGSZ> struct SubgridToGrid<2, SGSZ>
   inline static void FastCopy(std::vector<std::mutex> &m, Eigen::Array<int16_t, 2, 1> const corner, Cx4CMap const sx, Cx4Map x)
   {
     assert(m.size() == x.dimension(1));
-    for (Index ib = 0; ib < sx.dimension(3); ib++) {
-      for (Index ic = 0; ic < sx.dimension(2); ic++) {
-        for (Index iy = 0; iy < SGSZ; iy++) {
-          Index const      iiy = iy + corner[1];
-          std::scoped_lock lock(m[iiy]);
-          for (Index ix = 0; ix < SGSZ; ix++) {
-            Index const iix = ix + corner[0];
+    for (Index iy = 0; iy < SGSZ; iy++) {
+      Index const      iiy = iy + corner[1];
+      std::scoped_lock lock(m[iiy]);
+      for (Index ix = 0; ix < SGSZ; ix++) {
+        Index const iix = ix + corner[0];
+        for (Index ib = 0; ib < sx.dimension(3); ib++) {
+          for (Index ic = 0; ic < sx.dimension(2); ic++) {
             x(iix, iiy, ic, ib) += sx(ix, iy, ic, ib);
           }
         }
@@ -165,13 +166,13 @@ template <int SGSZ> struct SubgridToGrid<2, SGSZ>
   inline static void SlowCopy(std::vector<std::mutex> &m, Eigen::Array<int16_t, 2, 1> const corner, Cx4CMap const sx, Cx4Map x)
   {
     assert(m.size() == x.dimension(1));
-    for (Index ib = 0; ib < sx.dimension(3); ib++) {
-      for (Index ic = 0; ic < sx.dimension(2); ic++) {
-        for (Index iy = 0; iy < SGSZ; iy++) {
-          Index const      iiy = Wrap(iy + corner[1], x.dimension(1));
-          std::scoped_lock lock(m[iiy]);
-          for (Index ix = 0; ix < SGSZ; ix++) {
-            Index const iix = Wrap(ix + corner[0], x.dimension(0));
+    for (Index iy = 0; iy < SGSZ; iy++) {
+      Index const      iiy = Wrap(iy + corner[1], x.dimension(1));
+      std::scoped_lock lock(m[iiy]);
+      for (Index ix = 0; ix < SGSZ; ix++) {
+        Index const iix = Wrap(ix + corner[0], x.dimension(0));
+        for (Index ib = 0; ib < sx.dimension(3); ib++) {
+          for (Index ic = 0; ic < sx.dimension(2); ic++) {
             x(iix, iiy, ic, ib) += sx(ix, iy, ic, ib);
           }
         }
@@ -185,15 +186,15 @@ template <int SGSZ> struct SubgridToGrid<3, SGSZ>
   inline static void FastCopy(std::vector<std::mutex> &m, Eigen::Array<int16_t, 3, 1> const corner, Cx5CMap const sx, Cx5Map x)
   {
     assert(m.size() == x.dimension(2));
-    for (Index ib = 0; ib < sx.dimension(4); ib++) {
-      for (Index ic = 0; ic < sx.dimension(3); ic++) {
-        for (Index iz = 0; iz < SGSZ; iz++) {
-          Index const      iiz = iz + corner[2];
-          std::scoped_lock lock(m[iiz]);
-          for (Index iy = 0; iy < SGSZ; iy++) {
-            Index const iiy = iy + corner[1];
-            for (Index ix = 0; ix < SGSZ; ix++) {
-              Index const iix = ix + corner[0];
+    for (Index iz = 0; iz < SGSZ; iz++) {
+      Index const      iiz = iz + corner[2];
+      std::scoped_lock lock(m[iiz]);
+      for (Index iy = 0; iy < SGSZ; iy++) {
+        Index const iiy = iy + corner[1];
+        for (Index ix = 0; ix < SGSZ; ix++) {
+          Index const iix = ix + corner[0];
+          for (Index ib = 0; ib < sx.dimension(4); ib++) {
+            for (Index ic = 0; ic < sx.dimension(3); ic++) {
               x(iix, iiy, iiz, ic, ib) += sx(ix, iy, iz, ic, ib);
             }
           }
@@ -205,15 +206,15 @@ template <int SGSZ> struct SubgridToGrid<3, SGSZ>
   inline static void SlowCopy(std::vector<std::mutex> &m, Eigen::Array<int16_t, 3, 1> const corner, Cx5CMap const sx, Cx5Map x)
   {
     assert(m.size() == x.dimension(2));
-    for (Index ib = 0; ib < sx.dimension(4); ib++) {
-      for (Index ic = 0; ic < sx.dimension(3); ic++) {
-        for (Index iz = 0; iz < SGSZ; iz++) {
-          Index const      iiz = Wrap(iz + corner[2], x.dimension(2));
-          std::scoped_lock lock(m[iiz]);
-          for (Index iy = 0; iy < SGSZ; iy++) {
-            Index const iiy = Wrap(iy + corner[1], x.dimension(1));
-            for (Index ix = 0; ix < SGSZ; ix++) {
-              Index const iix = Wrap(ix + corner[0], x.dimension(0));
+    for (Index iz = 0; iz < SGSZ; iz++) {
+      Index const      iiz = Wrap(iz + corner[2], x.dimension(2));
+      std::scoped_lock lock(m[iiz]);
+      for (Index iy = 0; iy < SGSZ; iy++) {
+        Index const iiy = Wrap(iy + corner[1], x.dimension(1));
+        for (Index ix = 0; ix < SGSZ; ix++) {
+          Index const iix = Wrap(ix + corner[0], x.dimension(0));
+          for (Index ib = 0; ib < sx.dimension(4); ib++) {
+            for (Index ic = 0; ic < sx.dimension(3); ic++) {
               x(iix, iiy, iiz, ic, ib) += sx(ix, iy, iz, ic, ib);
             }
           }
