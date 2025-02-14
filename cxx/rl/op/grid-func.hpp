a15daa6bfae4befb997f55c31d88587284cf67ef
Tobias Wood <tobias@spinicist.org.uk>
2025-02-14 10:47:18 +0000

    Maps everywhere

--- cxx/rl/op/grid-func.hpp
+++ cxx/rl/op/grid-func.hpp
@@ -14,40 +14,40 @@ template <int FW> struct GFunc<1, FW>
   using KT = FixedTensor<float, 1, FW>;
 
   inline static void
-  Scatter(Eigen::Array<int16_t, 1, 1> const corner, int16_t const sample, int32_t const trace, KT const &k, Cx3CMap y, Cx3 &sg)
+  Scatter(Eigen::Array<int16_t, 1, 1> const c, int16_t const sample, int32_t const trace, KT const &k, Cx3CMap y, Cx3Map sg)
   {
     for (Index ic = 0; ic < y.dimension(0); ic++) {
       for (Index ix = 0; ix < FW; ix++) {
-        Index const iix = ix + corner[0] - FW / 2;
+        Index const iix = ix + c[0] - FW / 2;
         sg(iix, ic, 0) += y(ic, sample, trace) * k(ix);
       }
     }
   }
 
-  inline static void Gather(
-    Eigen::Array<int16_t, 1, 1> const corner, int16_t const sample, int32_t const trace, KT const &k, Cx3 const &sg, Cx3Map y)
+  inline static void
+  Gather(Eigen::Array<int16_t, 1, 1> const c, int16_t const sample, int32_t const trace, KT const &k, Cx3CMap sg, Cx3Map y)
   {
     for (Index ic = 0; ic < y.dimension(0); ic++) {
       for (Index ix = 0; ix < FW; ix++) {
-        Index const iix = ix + corner[0] - FW / 2;
+        Index const iix = ix + c[0] - FW / 2;
         y(ic, sample, trace) += sg(iix, ic, 0) * k(ix);
       }
     }
   }
 
   inline static void Scatter(Basis::CPtr                       basis,
-                             Eigen::Array<int16_t, 1, 1> const corner,
+                             Eigen::Array<int16_t, 1, 1> const c,
                              int16_t const                     sample,
                              int32_t const                     trace,
                              KT const                         &k,
                              Cx3CMap                           y,
-                             Cx3                              &sg)
+                             Cx3Map                            sg)
   {
     for (Index ib = 0; ib < basis->nB(); ib++) {
       auto const b = basis->entry(ib, sample, trace);
       for (Index ic = 0; ic < y.dimension(0); ic++) {
         for (Index ix = 0; ix < FW; ix++) {
-          Index const iix = ix + corner[0] - FW / 2;
+          Index const iix = ix + c[0] - FW / 2;
           sg(iix, ic, ib) += y(ic, sample, trace) * k(ix) * b;
         }
       }
@@ -55,18 +55,18 @@ template <int FW> struct GFunc<1, FW>
   }
 
   inline static void Gather(Basis::CPtr                       basis,
-                            Eigen::Array<int16_t, 1, 1> const corner,
+                            Eigen::Array<int16_t, 1, 1> const c,
                             int16_t const                     sample,
                             int32_t const                     trace,
                             KT const                         &k,
-                            Cx3 const                        &sg,
-                            Cx3Map                           &y)
+                            Cx3CMap                           sg,
+                            Cx3Map                            y)
   {
     for (Index ib = 0; ib < basis->nB(); ib++) {
       auto const b = basis->entry(ib, sample, trace);
       for (Index ic = 0; ic < y.dimension(0); ic++) {
         for (Index ix = 0; ix < FW; ix++) {
-          Index const iix = ix + corner[0] - FW / 2;
+          Index const iix = ix + c[0] - FW / 2;
           y(ic, sample, trace) += sg(iix, ic, ib) * k(ix) * b;
         }
       }
@@ -79,27 +79,27 @@ template <int FW> struct GFunc<2, FW>
   using KT = FixedTensor<float, 2, FW>;
 
   inline static void
-  Scatter(Eigen::Array<int16_t, 2, 1> const corner, int16_t const sample, int32_t const trace, KT const &k, Cx3CMap y, Cx4 &sg)
+  Scatter(Eigen::Array<int16_t, 2, 1> const c, int16_t const sample, int32_t const trace, KT const &k, Cx3CMap y, Cx4Map sg)
   {
     for (Index ic = 0; ic < y.dimension(0); ic++) {
       for (Index iy = 0; iy < FW; iy++) {
-        Index const iiy = iy + corner[1] - FW / 2;
+        Index const iiy = iy + c[1] - FW / 2;
         for (Index ix = 0; ix < FW; ix++) {
-          Index const iix = ix + corner[0] - FW / 2;
+          Index const iix = ix + c[0] - FW / 2;
           sg(iix, iiy, ic, 0) += y(ic, sample, trace) * k(ix, iy);
         }
       }
     }
   }
 
-  inline static void Gather(
-    Eigen::Array<int16_t, 2, 1> const corner, int16_t const sample, int32_t const trace, KT const &k, Cx4 const &sg, Cx3Map y)
+  inline static void
+  Gather(Eigen::Array<int16_t, 2, 1> const c, int16_t const sample, int32_t const trace, KT const &k, Cx4CMap sg, Cx3Map y)
   {
     for (Index ic = 0; ic < y.dimension(0); ic++) {
       for (Index iy = 0; iy < FW; iy++) {
-        Index const iiy = iy + corner[1] - FW / 2;
+        Index const iiy = iy + c[1] - FW / 2;
         for (Index ix = 0; ix < FW; ix++) {
-          Index const iix = ix + corner[0] - FW / 2;
+          Index const iix = ix + c[0] - FW / 2;
           y(ic, sample, trace) += sg(iix, iiy, ic, 0) * k(ix, iy);
         }
       }
@@ -107,20 +107,20 @@ template <int FW> struct GFunc<2, FW>
   }
 
   inline static void Scatter(Basis::CPtr                       basis,
-                             Eigen::Array<int16_t, 2, 1> const corner,
+                             Eigen::Array<int16_t, 2, 1> const c,
                              int16_t const                     sample,
                              int32_t const                     trace,
                              KT const                         &k,
                              Cx3CMap                           y,
-                             Cx4                              &sg)
+                             Cx4Map                            sg)
   {
     for (Index ib = 0; ib < basis->nB(); ib++) {
       auto const b = basis->entry(ib, sample, trace);
       for (Index ic = 0; ic < y.dimension(0); ic++) {
         for (Index iy = 0; iy < FW; iy++) {
-          Index const iiy = iy + corner[1] - FW / 2;
+          Index const iiy = iy + c[1] - FW / 2;
           for (Index ix = 0; ix < FW; ix++) {
-            Index const iix = ix + corner[0] - FW / 2;
+            Index const iix = ix + c[0] - FW / 2;
             sg(iix, iiy, ic, ib) += y(ic, sample, trace) * k(ix, iy) * b;
           }
         }
@@ -129,20 +129,20 @@ template <int FW> struct GFunc<2, FW>
   }
 
   inline static void Gather(Basis::CPtr                       basis,
-                            Eigen::Array<int16_t, 2, 1> const corner,
+                            Eigen::Array<int16_t, 2, 1> const c,
                             int16_t const                     sample,
                             int32_t const                     trace,
                             KT const                         &k,
-                            Cx4 const                        &sg,
-                            Cx3Map                           &y)
+                            Cx4CMap                           sg,
+                            Cx3Map                            y)
   {
     for (Index ib = 0; ib < basis->nB(); ib++) {
       auto const b = basis->entry(ib, sample, trace);
       for (Index ic = 0; ic < y.dimension(0); ic++) {
         for (Index iy = 0; iy < FW; iy++) {
-          Index const iiy = iy + corner[1] - FW / 2;
+          Index const iiy = iy + c[1] - FW / 2;
           for (Index ix = 0; ix < FW; ix++) {
-            Index const iix = ix + corner[0] - FW / 2;
+            Index const iix = ix + c[0] - FW / 2;
             y(ic, sample, trace) += sg(iix, iiy, ic, ib) * k(ix, iy) * b;
           }
         }
@@ -156,15 +156,15 @@ template <int FW> struct GFunc<3, FW>
   using KT = FixedTensor<float, 3, FW>;
 
   inline static void
-  Scatter(Eigen::Array<int16_t, 3, 1> const corner, int16_t const sample, int32_t const trace, KT const &k, Cx3CMap y, Cx5 &sg)
+  Scatter(Eigen::Array<int16_t, 3, 1> const c, int16_t const sample, int32_t const trace, KT const &k, Cx3CMap y, Cx5Map sg)
   {
     for (Index ic = 0; ic < y.dimension(0); ic++) {
       for (Index iz = 0; iz < FW; iz++) {
-        Index const iiz = iz + corner[2] - FW / 2;
+        Index const iiz = iz + c[2] - FW / 2;
         for (Index iy = 0; iy < FW; iy++) {
-          Index const iiy = iy + corner[1] - FW / 2;
+          Index const iiy = iy + c[1] - FW / 2;
           for (Index ix = 0; ix < FW; ix++) {
-            Index const iix = ix + corner[0] - FW / 2;
+            Index const iix = ix + c[0] - FW / 2;
             sg(iix, iiy, iiz, ic, 0) += y(ic, sample, trace) * k(ix, iy, iz);
           }
         }
@@ -172,16 +172,16 @@ template <int FW> struct GFunc<3, FW>
     }
   }
 
-  inline static void Gather(
-    Eigen::Array<int16_t, 3, 1> const corner, int16_t const sample, int32_t const trace, KT const &k, Cx5 const &sg, Cx3Map y)
+  inline static void
+  Gather(Eigen::Array<int16_t, 3, 1> const c, int16_t const sample, int32_t const trace, KT const &k, Cx5CMap sg, Cx3Map y)
   {
     for (Index ic = 0; ic < y.dimension(0); ic++) {
       for (Index iz = 0; iz < FW; iz++) {
-        Index const iiz = iz + corner[2] - FW / 2;
+        Index const iiz = iz + c[2] - FW / 2;
         for (Index iy = 0; iy < FW; iy++) {
-          Index const iiy = iy + corner[1] - FW / 2;
+          Index const iiy = iy + c[1] - FW / 2;
           for (Index ix = 0; ix < FW; ix++) {
-            Index const iix = ix + corner[0] - FW / 2;
+            Index const iix = ix + c[0] - FW / 2;
             y(ic, sample, trace) += sg(iix, iiy, iiz, ic, 0) * k(ix, iy, iz);
           }
         }
@@ -190,22 +190,22 @@ template <int FW> struct GFunc<3, FW>
   }
 
   inline static void Scatter(Basis::CPtr                       basis,
-                             Eigen::Array<int16_t, 3, 1> const corner,
+                             Eigen::Array<int16_t, 3, 1> const c,
                              int16_t const                     sample,
                              int32_t const                     trace,
                              KT const                         &k,
                              Cx3CMap                           y,
-                             Cx5                              &sg)
+                             Cx5Map                            sg)
   {
     for (Index ib = 0; ib < basis->nB(); ib++) {
       auto const b = basis->entry(ib, sample, trace);
       for (Index ic = 0; ic < y.dimension(0); ic++) {
         for (Index iz = 0; iz < FW; iz++) {
-          Index const iiz = iz + corner[2] - FW / 2;
+          Index const iiz = iz + c[2] - FW / 2;
           for (Index iy = 0; iy < FW; iy++) {
-            Index const iiy = iy + corner[1] - FW / 2;
+            Index const iiy = iy + c[1] - FW / 2;
             for (Index ix = 0; ix < FW; ix++) {
-              Index const iix = ix + corner[0] - FW / 2;
+              Index const iix = ix + c[0] - FW / 2;
               sg(iix, iiy, iiz, ic, ib) += y(ic, sample, trace) * k(ix, iy, iz) * b;
             }
           }
@@ -215,22 +215,22 @@ template <int FW> struct GFunc<3, FW>
   }
 
   inline static void Gather(Basis::CPtr                       basis,
-                            Eigen::Array<int16_t, 3, 1> const corner,
+                            Eigen::Array<int16_t, 3, 1> const c,
                             int16_t const                     sample,
                             int32_t const                     trace,
                             KT const                         &k,
-                            Cx5 const                        &sg,
-                            Cx3Map                           &y)
+                            Cx5CMap                           sg,
+                            Cx3Map                            y)
   {
     for (Index ib = 0; ib < basis->nB(); ib++) {
       auto const b = basis->entry(ib, sample, trace);
       for (Index ic = 0; ic < y.dimension(0); ic++) {
         for (Index iz = 0; iz < FW; iz++) {
-          Index const iiz = iz + corner[2] - FW / 2;
+          Index const iiz = iz + c[2] - FW / 2;
           for (Index iy = 0; iy < FW; iy++) {
-            Index const iiy = iy + corner[1] - FW / 2;
+            Index const iiy = iy + c[1] - FW / 2;
             for (Index ix = 0; ix < FW; ix++) {
-              Index const iix = ix + corner[0] - FW / 2;
+              Index const iix = ix + c[0] - FW / 2;
               y(ic, sample, trace) += sg(iix, iiy, iiz, ic, ib) * k(ix, iy, iz) * b;
             }
           }
