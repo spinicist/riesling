568d83a622bb42acdab721e1cda1190536575562
Tobias Wood <tobias@spinicist.org.uk>
2025-02-27 16:14:03 +0000

    Add a maps -> kernels option

--- cxx/riesling/sense/maps.cpp
+++ cxx/riesling/sense/maps.cpp
@@ -13,21 +13,29 @@ using namespace rl;
 
 void main_sense_maps(args::Subparser &parser)
 {
-  args::Positional<std::string> iname(parser, "FILE", "Input HD5 file"),
-    tname(parser, "FILE", "Target HD5 file for reconstruction"), oname(parser, "FILE", "Output HD5 file");
+  args::Positional<std::string> iname(parser, "INPUT", "Input HD5 file"),
+    tname(parser, "TARGET", "Target HD5 file for reconstruction"), oname(parser, "OUTPUT", "Output HD5 file");
 
   args::ValueFlag<float> osamp(parser, "O", "Grid oversampling factor (1.3)", {"osamp"}, 1.3f);
   ArrayFlag<float, 3>    fov(parser, "FOV", "Grid FoV in mm (x,y,z)", {"fov"}, Eigen::Array3f::Zero());
+  args::ValueFlag<Index> kW(parser, "W", "Turn maps into kernels with specified width", {"sense-width"});
 
   ParseCommand(parser, iname, oname);
   auto const  cmd = parser.GetCommand().Name();
-  HD5::Reader reader(tname.Get());
-  Trajectory  traj(reader, reader.readInfo().voxel_size);
+  HD5::Reader treader(tname.Get());
+  Trajectory  traj(treader, treader.readInfo().voxel_size);
 
-  HD5::Reader kreader(iname.Get());
-  Cx5 const   kernels = kreader.readTensor<Cx5>();
-  Cx5 const   maps = SENSE::KernelsToMaps(kernels, traj.matrixForFOV(fov.Get()), osamp.Get());
+  HD5::Reader reader(iname.Get());
   HD5::Writer writer(oname.Get());
-  writer.writeTensor(HD5::Keys::Data, maps.dimensions(), maps.data(), HD5::Dims::SENSE);
+  if (kW) {
+    Cx5 const maps = reader.readTensor<Cx5>();
+    Cx5 const kernels = SENSE::MapsToKernels(maps, kW.Get(), osamp.Get());
+    writer.writeTensor(HD5::Keys::Data, kernels.dimensions(), kernels.data(), HD5::Dims::SENSE);
+  } else {
+    Cx5 const kernels = reader.readTensor<Cx5>();
+    Cx5 const maps = SENSE::KernelsToMaps(kernels, traj.matrixForFOV(fov.Get()), osamp.Get());
+    writer.writeTensor(HD5::Keys::Data, maps.dimensions(), maps.data(), HD5::Dims::SENSE);
+  }
+
   Log::Print(cmd, "Finished");
 }
