25b1e5a19e7cc711788f7f335dd07f6266cfbb2a
Tobias Wood <tobias@spinicist.org.uk>
2025-03-25 11:27:21 +0000

    Tweaks to ZTE sim

--- cxx/rl/sim/zte.cpp
+++ cxx/rl/sim/zte.cpp
@@ -41,7 +41,7 @@ auto SegmentedZTE::offres(float const Δf) const -> Cx1
   return phase;
 }
 
-auto SegmentedZTE::readout(float const T2, float const Δf) const -> Cx1
+auto SegmentedZTE::readout(float const R2, float const Δf) const -> Cx1
 {
   if (p.samplesPerSpoke < 1) {
     Cx1 o(1);
@@ -49,7 +49,6 @@ auto SegmentedZTE::readout(float const T2, float const Δf) const -> Cx1
     return o;
   }
 
-  float const               R2 = 1.f / T2;
   Cx1                       e(p.samplesPerSpoke);
   Eigen::VectorXcf::MapType em(e.data(), p.samplesPerSpoke);
   float const               sampPhase = p.Tsamp * Δf * 2 * M_PI;
@@ -59,7 +58,7 @@ auto SegmentedZTE::readout(float const T2, float const Δf) const -> Cx1
   float const               startDecay = p.samplesGap * sampDecay;
   float const               endDecay = (p.samplesGap + p.samplesPerSpoke - 1) * sampDecay;
   em = Eigen::VectorXcf::LinSpaced(p.samplesPerSpoke, startPhase * 1if - startDecay, endPhase * 1if - endDecay).array().exp();
-  // Log::Print("Δf {} startPhase {} endPhase {}", Δf, startPhase, endPhase);
+  // fmt::print(stderr, "Δf {} startPhase {} endPhase {}", Δf, startPhase, endPhase);
   return e;
 }
 
@@ -73,9 +72,9 @@ auto SegmentedZTE::E(float const R, float const t) const -> Eigen::Matrix2f
 
 auto SegmentedZTE::ET2p(float const R2, float const t, bool const inv, float const q) const -> Eigen::Matrix2f
 {
-  float const e = (inv ? -1.f : 1.f) * exp(-R2 * t);
-  float const qq = (q + 1.f) / 2.f; // q is defined as -1 to 1, need to convert to 0 to 1
-  float const eq = e * qq + 1.f * (1.f - qq);
+  float const     e = (inv ? -1.f : 1.f) * exp(-R2 * t);
+  float const     qq = (q + 1.f) / 2.f; // q is defined as -1 to 1, need to convert to 0 to 1
+  float const     eq = e * qq + 1.f * (1.f - qq);
   Eigen::Matrix2f Et;
   Et << eq, 0.f, 0.f, 1.f;
   return Et;
@@ -138,23 +137,25 @@ auto SegmentedZTE::nTissueParameters() const -> Index { return 4; }
 
 auto SegmentedZTE::simulate(Eigen::ArrayXf const &pars) const -> Cx2
 {
-  if (pars.size() != 3) { throw Log::Failure("Sim", "Need 3 parameters T1 T2 Δf"); }
-  float const R1 = 1.f / pars(0);
-  // float const R2s = 1.f / pars(1);
-  float const Δf = pars(2);
-  float const B1 = pars(3);
+  if (pars.size() != nTissueParameters()) {
+    throw Log::Failure("Sim", "Need {} parameters M0 T1 T2 Δf, received [{:.3f}]", nTissueParameters(), fmt::join(pars, ","));
+  }
+  float const M0 = pars(0);
+  float const R1 = 1.f / pars(1);
+  float const R2 = 1.f / pars(2);
+  float const Δf = pars(3);
 
   // Get steady state before first read-out
-  Eigen::Matrix2f const seg = Eseg(R1, B1);
+  Eigen::Matrix2f const seg = Eseg(R1, 1.f);
   float const           m_ss = seg(0, 1) / (1.f - seg(0, 0));
 
   // Now fill in dynamic
   Index           tp = 0;
   Cx1             s0(traces());
-  Eigen::Vector2f Mz{m_ss, 1.f};
-  segment(tp, Mz, s0, R1, B1);
+  Eigen::Vector2f Mz{m_ss * M0, 1.f};
+  segment(tp, Mz, s0, R1, 1.f);
   if (tp != traces()) { throw Log::Failure("Sim", "Programmer error"); }
-  return readout(pars(1), Δf).contract(s0, Eigen::array<Eigen::IndexPair<Index>, 0>());
+  return readout(R2, Δf).contract(s0, Eigen::array<Eigen::IndexPair<Index>, 0>());
 }
 
 auto SegmentedZTE::timepoints() const -> Re1
