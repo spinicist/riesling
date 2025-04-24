64c17413d66bbc3cf722284a54a5daa9d88a654d
Tobias Wood <tobias@spinicist.org.uk>
2025-04-24 16:29:43 +0100

    Add off-resonance to rlsq

--- cxx/riesling/recon/rlsq.cpp
+++ cxx/riesling/recon/rlsq.cpp
@@ -36,9 +36,11 @@ void main_recon_rlsq(args::Subparser &parser)
   auto        noncart = reader.readTensor<Cx5>();
   traj.checkDims(FirstN<3>(noncart.dimensions()));
 
-  auto const  basis = LoadBasis(coreArgs.basisFile.Get());
-  auto const  R = Recon(reconArgs.Get(), preArgs.Get(), gridArgs.Get(), senseArgs.Get(), traj, basis.get(), noncart);
-  auto const  shape = R.A->ishape;
+  auto const basis = LoadBasis(coreArgs.basisFile.Get());
+  auto const R = f0Args.Nτ ? Recon(reconArgs.Get(), preArgs.Get(), gridArgs.Get(), senseArgs.Get(), traj, f0Args.Get(), noncart,
+                                   reader.readTensor<Re3>("f0map"))
+                           : Recon(reconArgs.Get(), preArgs.Get(), gridArgs.Get(), senseArgs.Get(), traj, basis.get(), noncart);
+  auto const shape = R.A->ishape;
   float const scale = ScaleData(scaling.Get(), R.A, R.M, CollapseToVector(noncart));
   if (scale != 1.f) { noncart.device(Threads::TensorDevice()) = noncart * Cx(scale); }
   auto [reg, A, ext_x] = Regularizers(regOpts, R.A);
