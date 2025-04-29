5c549290744f46e363e06d9d468e6155881be138
Tobias Wood <tobias@spinicist.org.uk>
2025-04-29 21:32:16 +0100

    cuTensor skeleton

--- cxx/furmint/dft.cu
+++ cxx/furmint/dft.cu
@@ -10,49 +10,99 @@
 
 using namespace rl;
 
+#define HANDLE_ERROR(x)                                                                                                        \
+  {                                                                                                                            \
+    const auto err = x;                                                                                                        \
+    if (err != CUTENSOR_STATUS_SUCCESS) { throw Log::Failure("CUTENSOR", "Error: {}", cutensorGetErrorString(err)) }           \
+  };
+
+#define HANDLE_CUDA_ERROR(x)                                                                                                   \
+  {                                                                                                                            \
+    const auto err = x;                                                                                                        \
+    if (err != cudaSuccess) { throw Log::Failure("CUDA", "Error: {}", cudaGetErrorString(err)); }                              \
+  };
+
 void main_ndft(args::Subparser &parser)
 {
-  CoreArgs    coreArgs(parser);
-  GridArgs<3> gridArgs(parser);
-  PreconArgs  preArgs(parser);
-  LSMRArgs    lsqOpts(parser);
-
-  args::Flag fwd(parser, "", "Apply forward operation", {'f', "fwd"});
-  args::Flag adj(parser, "", "Apply adjoint operation", {'a', "adj"});
+  CoreArgs coreArgs(parser);
   ParseCommand(parser, coreArgs.iname, coreArgs.oname);
 
   HD5::Reader reader(coreArgs.iname.Get());
   Info const  info = reader.readInfo();
   auto const  shape = reader.dimensions();
-  auto const  basis = LoadBasis(coreArgs.basisFile.Get());
+  auto const  nC = shape[0];
+  auto const  nSamp = shape[1];
+  auto const  nTrace = shape[2];
   HD5::Writer writer(coreArgs.oname.Get());
   writer.writeInfo(info);
-
   Trajectory traj(reader, info.voxel_size, coreArgs.matrix.Get());
-  if (fwd) {
-    auto const cart = reader.readTensor<Cx6>();
-    auto const nC = shape[3];
-    auto const nS = 1;
-    auto const nT = shape[5];
-    auto const ndft = TOps::NDFT<3>::Make(traj.matrixForFOV(gridArgs.fov.Get()), traj.points(), nC, basis.get());
-    auto const A = Loopify<TOps::NDFT<3>>(ndft, nS, nT);
-    auto const noncart = A->forward(cart);
-    writer.writeTensor(HD5::Keys::Data, noncart.dimensions(), noncart.data(), HD5::Dims::Noncartesian);
-  } else {
-    auto const noncart = reader.readTensor<Cx5>();
-    auto const nC = shape[0];
-    auto const nS = shape[3];
-    auto const nT = shape[4];
-    auto const ndft = TOps::NDFT<3>::Make(traj.matrixForFOV(gridArgs.fov.Get()), traj.points(), nC, basis.get());
-    auto const A = Loopify<TOps::NDFT<3>>(ndft, nS, nT);
-    if (adj) {
-      auto const cart = A->adjoint(noncart);
-      writer.writeTensor(HD5::Keys::Data, cart.dimensions(), cart.data(), HD5::Dims::Channels);
-    } else {
-      auto const M = ndft->M(preArgs.λ.Get(), nS, nT);
-      LSMR const lsmr{A, M, nullptr, lsqOpts.Get()};
-      auto const c = lsmr.run(CollapseToConstVector(noncart));
-      writer.writeTensor(HD5::Keys::Data, A->ishape, c.data(), HD5::Dims::Channels);
+  auto const mat = traj.matrix();
+  Cx5 const  noncart = reader.readTensor<Cx5>();
+  Cx4 const  images(AddBack(mat, nC));
+  Re3 const  ncTraj = traj.points();
+  Re4        cTraj(3, mat[0], mat[1], mat[2]);
+
+  for (Index ik = 0; ik < mat[2]; ik++) {
+    float const fk = (2.f * M_PI * (ik - mat[2] / 2)) / mat[2];
+    for (Index ij = 0; ij < mat[1]; ij++) {
+      float const fj = (2.f * M_PI * (ij - mat[1] / 2)) / mat[1];
+      for (Index ii = 0; ii < mat[0]; ii++) {
+        float const fi = (2.f * M_PI * (ii - mat[0] / 2)) / mat[0];
+        cTraj(0, ii, ij, ik) = fi;
+        cTraj(1, ii, ij, ik) = fj;
+        cTraj(2, ii, ij, ik) = fk;
+      }
     }
   }
+
+  std::vector<int> mNCimg{'c', 's', 't'};
+  std::vector<int> mCimg{'i', 'j', 'k', 'c'};
+  std::vector<int> mE('i','j','k');
+  std::vector<int> mEt('s','t');
+  std::vector<int> mNCTraj{'d', 's', 't'};
+  std::vector<int> mCTraj{'d', 'i', 'j', 'k'};
+
+  std::unordered_map<int, int64_t> xNCimg, xCimg, xNCTraj, xCTraj, xE;
+  xNCimg['c'] = nC;
+  xNCimg['s'] = nSamp;
+  xNCimg['t'] = nTrace;
+  xCimg['i'] = mat[0];
+  xCimg['j'] = mat[1];
+  xCimg['k'] = mat[2];
+  xCimg['c'] = nC;
+  xNCTraj['d'] = 3;
+  xNCTraj['s'] = nSamp;
+  xNCTraj['t'] = nTrace;
+  xCTraj['d'] = 3;
+  xCTraj['i'] = mat[0];
+  xCTraj['j'] = mat[1];
+  xCTraj['k'] = mat[2];
+  xE['i'] = mat[0];
+  xE['j'] = mat[1];
+  xE['k'] = mat[2];
+  xEt['s'] = nSamp;
+  xEt['t'] = nTrace;
+
+  void *gpuNCimg, *gpuCimg, *gpuNCTraj, *gpuCTraj;
+
+  HANDLE_CUDA_ERROR(cudaMalloc((void **)&gpuNCimg, mNCimg.size() * sizeof(Cx)));
+  HANDLE_CUDA_ERROR(cudaMalloc((void **)&gpuCimg, mCimg.size() * sizeof(Cx)));
+  HANDLE_CUDA_ERROR(cudaMalloc((void **)&gpuNCTraj, ncTraj.size() * sizeof(Re)));
+  HANDLE_CUDA_ERROR(cudaMalloc((void **)&gpuCTraj, cTraj.size() * sizeof(Re)));
+
+  HANDLE_CUDA_ERROR(cudaMemcpy(gpuNCimg, noncart.data(), cudaMemcpyHostToDevice));
+  HANDLE_CUDA_ERROR(cudaMemcpy(gpuNCTraj, ncTraj.data(), cudaMemcpyHostToDevice));
+  HANDLE_CUDA_ERROR(cudaMemcpy(gpuCTraj, cTraj.data(), cudaMemcpyHostToDevice));
+
+  cutensorHandle_t handle;
+  HANDLE_ERROR(cutensorCreate(&handle));
+
+  cutensorTensorDescriptor_t dNCimg, dCimg, dNCTraj, dCTraj;
+  uint32_t const kAlignment = 128;
+  HANDLE_ERROR(cutensorCreateTensorDescriptor(handle, &dNCimg, mNCimg, xNCimg.data(), NULL, CUTENSOR_C_32F, kAlignment));
+  HANDLE_ERROR(cutensorCreateTensorDescriptor(handle, &dCimg, mCimg, xCimg.data(), NULL, CUTENSOR_C_32F, kAlignment));
+  HANDLE_ERROR(cutensorCreateTensorDescriptor(handle, &dNCTraj, mNCTraj, xNCTraj.data(), NULL, CUTENSOR_R_32F, kAlignment));
+  HANDLE_ERROR(cutensorCreateTensorDescriptor(handle, &dCTraj, mCTraj, xCTraj.data(), NULL, CUTENSOR_R_32F, kAlignment));
+
+  writer.writeTensor(HD5::Keys::Data, cart.dimensions(), cart.data(), HD5::Dims::Channels);
 }
