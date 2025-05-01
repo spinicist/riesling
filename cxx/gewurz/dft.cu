c95f6840481bf56ce002a893a133a74df1ab3368
Tobias Wood <tobias.wood@kcl.ac.uk>
2025-05-01 15:39:18 +0100

    Prep for half precision

--- cxx/gewurz/dft.cu
+++ cxx/gewurz/dft.cu
@@ -5,9 +5,10 @@
 #include "rl/types.hpp"
 
 #define LIBCUDACXX_ENABLE_SIMPLIFIED_COMPLEX_OPERATIONS
-#include "math_constants.h"
 #include <cuda/std/complex>
 #include <cuda/std/mdspan>
+#include <cuda_fp16.h>
+#include <math_constants.h>
 #include <thrust/copy.h>
 #include <thrust/device_vector.h>
 #include <thrust/host_vector.h>
@@ -19,16 +20,18 @@ template <int N> using ExtN = cuda::std::dextents<int, N>;
 using Ext3 = ExtN<3>;
 using Ext4 = ExtN<4>;
 
-template <int N> using ReSN = cuda::std::mdspan<float, ExtN<N>, cuda::std::layout_left>;
+using RealType = float;
+
+template <int N> using ReSN = cuda::std::mdspan<RealType, ExtN<N>, cuda::std::layout_left>;
 using ReS3 = ReSN<3>;
 using ReS4 = ReSN<4>;
 
-using CCx = cuda::std::complex<float>;
+using CCx = cuda::std::complex<RealType>;
 template <int N> using CxSN = cuda::std::mdspan<CCx, ExtN<N>, cuda::std::layout_left>;
 using CxS3 = CxSN<3>;
 using CxS4 = CxSN<4>;
 
-using ReVec = thrust::universal_vector<float>;
+using ReVec = thrust::universal_vector<RealType>;
 using CxVec = thrust::universal_vector<CCx>;
 
 void ForwardDFT(CxS4 imgs, ReS3 traj, CxS3 ks)
@@ -41,6 +44,8 @@ void ForwardDFT(CxS4 imgs, ReS3 traj, CxS3 ks)
 
   thrust::fill_n(ks.data_handle(), ks.size(), 0.f);
   thrust::for_each_n(thrust::cuda::par, thrust::make_counting_iterator(0), nST, [imgs, traj, ks] __device__(int st) {
+    RealType const pi2 = RealType(2.f * CUDART_PI_F);
+
     int const nI = imgs.extent(0);
     int const nJ = imgs.extent(1);
     int const nK = imgs.extent(2);
@@ -53,10 +58,10 @@ void ForwardDFT(CxS4 imgs, ReS3 traj, CxS3 ks)
     for (int ii = 0; ii < nI; ii++) {
       for (int ij = 0; ij < nJ; ij++) {
         for (int ik = 0; ik < nK; ik++) {
-          float const phase = 2.f * CUDART_PI_F * traj(0, is, it) * (ii - nI / 2.f) / nI +
-                              2.f * CUDART_PI_F * traj(1, is, it) * (ij - nJ / 2.f) / nJ +
-                              2.f * CUDART_PI_F * traj(2, is, it) * (ik - nK / 2.f) / nK;
-          CCx const ep(cuda::std::cos(-phase), cuda::std::sin(-phase));
+          float const p = pi2 * traj(0, is, it) * (RealType)(ii - nI / 2) / (RealType)nI +
+                          pi2 * traj(1, is, it) * (RealType)(ij - nJ / 2) / (RealType)nJ +
+                          pi2 * traj(2, is, it) * (RealType)(ik - nK / 2) / (RealType)nK;
+          CCx const ep(cuda::std::cos(-p), cuda::std::sin(-p));
           for (int ic = 0; ic < ks.extent(0); ic++) {
             ks(ic, is, it) += ep * imgs(ii, ij, ik, ic);
           }
@@ -79,6 +84,8 @@ void AdjointDFT(CxS3 ks, ReS3 traj, CxS4 imgs)
   auto const start = Log::Now();
   thrust::fill_n(imgs.data_handle(), imgs.size(), 0.f);
   thrust::for_each_n(thrust::cuda::par, thrust::make_counting_iterator(0), nIJK, [imgs, traj, ks] __device__(int ijk) {
+    RealType const pi2 = RealType(2.f * CUDART_PI_F);
+
     int const nI = imgs.extent(0);
     int const nJ = imgs.extent(1);
     int const nK = imgs.extent(2);
@@ -92,14 +99,12 @@ void AdjointDFT(CxS3 ks, ReS3 traj, CxS4 imgs)
 
     for (int it = 0; it < nT; it++) {
       for (int is = 0; is < nS; is++) {
-        float const p = 2.f * CUDART_PI_F * traj(0, is, it) * (ii - nI / 2.f) / nI +
-                        2.f * CUDART_PI_F * traj(1, is, it) * (ij - nJ / 2.f) / nJ +
-                        2.f * CUDART_PI_F * traj(2, is, it) * (ik - nK / 2.f) / nK;
+        float const p = pi2 * traj(0, is, it) * (RealType)(ii - nI / 2) / (RealType)nI +
+                        pi2 * traj(1, is, it) * (RealType)(ij - nJ / 2) / (RealType)nJ +
+                        pi2 * traj(2, is, it) * (RealType)(ik - nK / 2) / (RealType)nK;
         CCx const ep(cuda::std::cos(p), cuda::std::sin(p));
-        // fmt::print(stderr, "ijk {} {} {} st {} {} p {} ep {} {}\n", ii, ij, ik, is, it, p, ep.real(), ep.imag());
         for (int ic = 0; ic < ks.extent(0); ic++) {
           imgs(ii, ij, ik, ic) += ep * ks(ic, is, it);
-          // imgs(ii, ij, ik, ic) = ic + 1;
         }
       }
     }
@@ -119,6 +124,8 @@ void AdjointDFT(CxS3 ks, ReS3 M, ReS3 traj, CxS4 imgs)
   auto const start = Log::Now();
   thrust::fill_n(imgs.data_handle(), imgs.size(), 0.f);
   thrust::for_each_n(thrust::cuda::par, thrust::make_counting_iterator(0), nIJK, [ks, M, traj, imgs] __device__(int ijk) {
+    RealType const pi2 = RealType(2.f * CUDART_PI_F);
+
     int const nI = imgs.extent(0);
     int const nJ = imgs.extent(1);
     int const nK = imgs.extent(2);
@@ -132,9 +139,9 @@ void AdjointDFT(CxS3 ks, ReS3 M, ReS3 traj, CxS4 imgs)
 
     for (int it = 0; it < nT; it++) {
       for (int is = 0; is < nS; is++) {
-        float const p = 2.f * CUDART_PI_F * traj(0, is, it) * (ii - nI / 2.f) / nI +
-                        2.f * CUDART_PI_F * traj(1, is, it) * (ij - nJ / 2.f) / nJ +
-                        2.f * CUDART_PI_F * traj(2, is, it) * (ik - nK / 2.f) / nK;
+        float const p = pi2 * traj(0, is, it) * (RealType)(ii - nI / 2) / (RealType)nI +
+                        pi2 * traj(1, is, it) * (RealType)(ij - nJ / 2) / (RealType)nJ +
+                        pi2 * traj(2, is, it) * (RealType)(ik - nK / 2) / (RealType)nK;
         CCx const ep(cuda::std::cos(p), cuda::std::sin(p));
         // fmt::print(stderr, "ijk {} {} {} st {} {} p {} ep {} {}\n", ii, ij, ik, is, it, p, ep.real(), ep.imag());
         for (int ic = 0; ic < ks.extent(0); ic++) {
@@ -182,13 +189,14 @@ void main_dft(args::Subparser &parser)
   AdjointDFT(sMKS, sT, sImg);
   ForwardDFT(sImg, sT, sMKS);
   thrust::transform(thrust::cuda::par, vMKS.begin(), vMKS.end(), vM.begin(),
-                    [] __device__(CCx x) { return 1.f / cuda::std::abs(x); });
+                    [] __device__(CCx x) { return (RealType)1 / cuda::std::abs(x); });
 
   Log::Print("gewurz", "Recon");
   thrust::copy_n(input.data(), nC * nS * nT, vKS.begin());
   AdjointDFT(sKS, sM, sT, sImg);
 
   Cx6 output(mat[0], mat[1], mat[2], nC, 1, 1);
+  // thrust::transform(thrust::host, vImg.begin(), vImg.end(), output.data(), [](CCx x) { return Cx(x.real(), x.imag()); });
   thrust::copy_n(vImg.begin(), vImg.size(), output.data());
   HD5::Writer writer(coreArgs.oname.Get());
   writer.writeInfo(info);
