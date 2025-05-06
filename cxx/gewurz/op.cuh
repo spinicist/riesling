3e2b18fbc798a752a7f75a86ab30612dc1e74af0
Tobias Wood <tobias.wood@kcl.ac.uk>
2025-05-06 18:27:37 +0100

    Fix enough to get a wrong image

--- cxx/gewurz/op.cuh
+++ cxx/gewurz/op.cuh
@@ -19,14 +19,6 @@ template <typename T, typename MT, int Rank> struct MulPacked : Op<T, Rank, Rank
   using FMTensor = DTensor<MT, 1>::Span;
   MTensor const m;
 
-  struct MulKernel {
-    int M, N;
-    __host__ __device__ __half operator()(float const f) const
-    {
-      return __float2half(f);
-    }
-  }
-
   MulPacked(MTensor mm)
     : m(mm) {};
   void forward(XTensor x, XTensor y) const override
