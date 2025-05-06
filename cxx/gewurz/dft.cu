68f8cdcd0f4088082c3437705675f581eecd3334
Tobias Wood <tobias.wood@kcl.ac.uk>
2025-05-06 14:57:42 +0100

    Add local accumulate space

--- cxx/gewurz/dft.cu
+++ cxx/gewurz/dft.cu
@@ -30,24 +30,25 @@ void ThreeD::forward(DTensor<CuCxF, 3>::Span imgs, DTensor<CuCxF, 2>::Span ks) c
                      [imgs, traj = this->traj, ks, scale] __device__(int st) {
                        CuReal const pi2 = CuReal(2.f * CUDART_PI_F);
 
+                       int const    nS = ks.extent(0);
+                       int const    nT = ks.extent(1);
+                       int const    it = st / nS;
+                       int const    is = st % nS;
+                       float3 const k{traj(0, is, it), traj(1, is, it), traj(2, is, it)};
+                       ks(is, it) = 0.f;
+
                        int const nI = imgs.extent(0);
                        int const nJ = imgs.extent(1);
                        int const nK = imgs.extent(2);
-                       int const nS = ks.extent(0);
-                       int const nT = ks.extent(1);
-
-                       int const it = st / nS;
-                       int const is = st % nS;
-
-                       ks(is, it) = 0.f;
-                       for (int ii = 0; ii < nI; ii++) {
+                       for (int ik = 0; ik < nK; ik++) {
+                         float const z = (ik - nK / 2.f) / (float)nK;
                          for (int ij = 0; ij < nJ; ij++) {
-                           for (int ik = 0; ik < nK; ik++) {
-                             float const p = pi2 * (traj(0, is, it) * (CuReal)(ii - nI / 2) / (CuReal)nI +
-                                                    traj(1, is, it) * (CuReal)(ij - nJ / 2) / (CuReal)nJ +
-                                                    traj(2, is, it) * (CuReal)(ik - nK / 2) / (CuReal)nK);
-                             CuCxF const ep(cuda::std::cos(-p), cuda::std::sin(-p));
-                             ks(is, it) += ep * scale * imgs(ii, ij, ik);
+                           float const y = (ij - nJ / 2.f) / (float)nJ;
+                           for (int ii = 0; ii < nI; ii++) {
+                             float const x = (ii - nI / 2.f) / (float)nI;
+                             float const p = pi2 * (k.x * x + k.y * y + k.z * z);
+                             CuCxF const ep(scale * cuda::std::cos(-p), scale * cuda::std::sin(-p));
+                             ks(is, it) += ep * imgs(ii, ij, ik);
                            }
                          }
                        }
@@ -84,21 +85,26 @@ void ThreeD::adjoint(DTensor<CuCxF, 2>::Span ks, DTensor<CuCxF, 3>::Span imgs) c
 
     imgs(ii, ij, ik) = 0.f;
 
+    float3 const r{(ii - nI / 2.f) / (float)nI, (ij - nJ / 2.f) / (float)nJ, (ik - nK / 2.f) / (float)nK};
+
     for (int it = 0; it < nT; it++) {
       for (int is = 0; is < nS; is++) {
-        float const p =
-          pi2 * (traj(0, is, it) * (CuReal)(ii - nI / 2) / (CuReal)nI + traj(1, is, it) * (CuReal)(ij - nJ / 2) / (CuReal)nJ +
-                 traj(2, is, it) * (CuReal)(ik - nK / 2) / (CuReal)nK);
-        CuCxF const ep(cuda::std::cos(p), cuda::std::sin(p));
-        imgs(ii, ij, ik) += ep * scale * ks(is, it);
+        float3 const k{traj(0, is, it), traj(1, is, it), traj(2, is, it)};
+        float const  p = pi2 * (r.x * k.x + r.y * k.y + r.z * k.z);
+        CuCxF const  ep(scale * cuda::std::cos(p), scale * cuda::std::sin(p));
+        imgs(ii, ij, ik) += ep * ks(is, it);
       }
     }
   });
   rl::Log::Print("DFT", "Adjoint DFT finished in {}", rl::Log::ToNow(start));
 }
 
+template <int NP>
 void ThreeDPacked::forward(DTensor<CuCxF, 4>::Span imgs, DTensor<CuCxF, 3>::Span ks) const
 {
+  if (NP != imgs.extent(0) || NP != ks.extent(0)) {
+    throw rl::Log::Failure("DFT", "Packing dimension size mismatch");
+  }
   rl::Log::Print("DFT", "Forward Packed DFT");
   auto const  start = rl::Log::Now();
   int const   nS = ks.extent(1);
@@ -109,45 +115,47 @@ void ThreeDPacked::forward(DTensor<CuCxF, 4>::Span imgs, DTensor<CuCxF, 3>::Span
   int const   nK = imgs.extent(3);
   int const   nIJK = nI * nJ * nK;
   float const scale = 1.f / cuda::std::sqrt(nIJK);
+  auto        it = thrust::make_counting_iterator(0);
+  thrust::for_each_n(thrust::cuda::par, it, nST, [imgs, traj = this->traj, ks, scale] __device__(int st) {
+    CuReal const pi2 = CuReal(2.f * CUDART_PI_F);
+    int const    nC = ks.extent(0);
+    int const    nS = ks.extent(1);
+    int const    nT = ks.extent(2);
+    int const    it = st / nS;
+    int const    is = st % nS;
+    float3 const k{traj(0, is, it), traj(1, is, it), traj(2, is, it)};
 
-  thrust::for_each_n(thrust::cuda::par, thrust::make_counting_iterator(0), nST,
-                     [imgs, traj = this->traj, ks, scale] __device__(int st) {
-                       CuReal const pi2 = CuReal(2.f * CUDART_PI_F);
-                       int const    nC = imgs.extent(0);
-                       int const    nI = imgs.extent(1);
-                       int const    nJ = imgs.extent(2);
-                       int const    nK = imgs.extent(3);
-                       int const    nS = ks.extent(1);
-                       int const    nT = ks.extent(2);
-
-                       int const it = st / nS;
-                       int const is = st % nS;
-
-                       for (int ic = 0; ic < nC; ic++) {
-                         ks(ic, is, it) = 0.f;
-                       }
-
-                       for (int ii = 0; ii < nI; ii++) {
-                         for (int ij = 0; ij < nJ; ij++) {
-                           for (int ik = 0; ik < nK; ik++) {
-                             float const p = pi2 * (traj(0, is, it) * (CuReal)(ii - nI / 2) / (CuReal)nI +
-                                                    traj(1, is, it) * (CuReal)(ij - nJ / 2) / (CuReal)nJ +
-                                                    traj(2, is, it) * (CuReal)(ik - nK / 2) / (CuReal)nK);
-                             CuCxF const ep(cuda::std::cos(-p), cuda::std::sin(-p));
-                             for (int ic = 0; ic < nC; ic++) {
-                               ks(ic, is, it) += ep * scale * imgs(ic, ii, ij, ik);
-                             }
-                           }
-                         }
-                       }
-                     });
+    int const nI = imgs.extent(1);
+    int const nJ = imgs.extent(2);
+    int const nK = imgs.extent(3);
+    CuCxF temp[NP] = {CuCxF(0.), };
+    for (int ik = 0; ik < nK; ik++) {
+      float const z = (ik - nK / 2.f) / (float)nK;
+      for (int ij = 0; ij < nJ; ij++) {
+        float const y = (ij - nJ / 2.f) / (float)nJ;
+        for (int ii = 0; ii < nI; ii++) {
+          float const x = (ii - nI / 2.f) / (float)nI;
+          float const p = pi2 * (k.x * x + k.y * y + k.z * z);
+          CuCxF const ep(scale * cuda::std::cos(-p), scale * cuda::std::sin(-p));
+          for (int ic = 0; ic < NP; ic++) {
+            temp[ic] += ep * imgs(ic, ii, ij, ik);
+          }
+        }
+      }
+    }
+    for (int ic = 0; ic < NP; ic++) {
+      ks(ic, is, it) = temp[ic];
+    }
+  });
   rl::Log::Print("DFT", "Forward Packed DFT finished in {}", rl::Log::ToNow(start));
 }
 
+template<int NP>
 void ThreeDPacked::adjoint(DTensor<CuCxF, 3>::Span ks, DTensor<CuCxF, 4>::Span imgs) const
 {
-  int const nC = imgs.extent(0);
-  if (nC != ks.extent(0)) { throw rl::Log::Failure("DFT", "Image had {} channels k-space had {}", nC, ks.extent(0)); }
+  if (NP != imgs.extent(0) || NP != ks.extent(0)) {
+    throw rl::Log::Failure("DFT", "Packing dimension size mismatch");
+  }
   int const   nI = imgs.extent(1);
   int const   nJ = imgs.extent(2);
   int const   nK = imgs.extent(3);
@@ -157,7 +165,8 @@ void ThreeDPacked::adjoint(DTensor<CuCxF, 3>::Span ks, DTensor<CuCxF, 4>::Span i
   rl::Log::Print("DFT", "Adjoint Packed DFT {} {} {} -> {} {} {} {} T {} {} {}", ks.extent(0), ks.extent(1), ks.extent(2),
                  imgs.extent(0), nI, nJ, nK, traj.extent(0), traj.extent(1), traj.extent(2));
   auto const start = rl::Log::Now();
-  auto       it = thrust::make_counting_iterator(0);
+
+  auto it = thrust::make_counting_iterator(0);
   thrust::for_each_n(thrust::cuda::par, it, nIJK, [ks, traj = this->traj, imgs, scale] __device__(int ijk) {
     CuReal const pi2 = CuReal(2.f * CUDART_PI_F);
     int const    nC = imgs.extent(0);
@@ -172,22 +181,25 @@ void ThreeDPacked::adjoint(DTensor<CuCxF, 3>::Span ks, DTensor<CuCxF, 4>::Span i
     int const ij = ijk % nIJ / nI;
     int const ii = ijk % nIJ % nI;
 
-    for (int ic = 0; ic < nC; ic++) {
-      imgs(ic, ii, ij, ik) = 0.f;
-    }
-
+    CuCxF        temp[NP] = {CuCxF(0.f),};
+    float3 const r{(ii - nI / 2.f) / (float)nI, (ij - nJ / 2.f) / (float)nJ, (ik - nK / 2.f) / (float)nK};
     for (int it = 0; it < nT; it++) {
       for (int is = 0; is < nS; is++) {
-        float const p =
-          pi2 * (traj(0, is, it) * (CuReal)(ii - nI / 2) / (CuReal)nI + traj(1, is, it) * (CuReal)(ij - nJ / 2) / (CuReal)nJ +
-                 traj(2, is, it) * (CuReal)(ik - nK / 2) / (CuReal)nK);
-        CuCxF const ep(cuda::std::cos(p), cuda::std::sin(p));
-        for (int ic = 0; ic < nC; ic++) {
-          imgs(ic, ii, ij, ik) += ep * scale * ks(ic, is, it);
+        float3 const k{traj(0, is, it), traj(1, is, it), traj(2, is, it)};
+        float const  p = pi2 * (r.x * k.x + r.y * k.y + r.z * k.z);
+        CuCxF const  ep(scale * cuda::std::cos(p), scale * cuda::std::sin(p));
+        for (int ic = 0; ic < NP; ic++) {
+          temp[ic] += ep * ks(ic, is, it);
         }
       }
+    }
+    for (int ic = 0; ic < NP; ic++) {
+      imgs(ic, ii, ij, ik) = temp[ic];
     }
   });
   rl::Log::Print("DFT", "Adjoint Packed DFT finished in {}", rl::Log::ToNow(start));
 }
+
+template struct DFT<8>;
+
 } // namespace gw::DFT
--- cxx/gewurz/dft.cuh
+++ cxx/gewurz/dft.cuh
@@ -13,18 +13,20 @@ struct ThreeD : Op<CuCxF, 3, 2>
   using YT = DTensor<CuCxF, 2>::Span;
   using TT = DTensor<float, 3>::Span;
   TT traj;
-  ThreeD(TT t) : traj(t) {};
+  ThreeD(TT t)
+    : traj(t) {};
   void forward(XT x, YT y) const override;
   void adjoint(YT y, XT x) const override;
 };
 
-struct ThreeDPacked : Op<CuCxF, 4, 3>
+template <int NP> struct ThreeDPacked : Op<CuCxF, 4, 3>
 {
   using XT = DTensor<CuCxF, 4>::Span;
   using YT = DTensor<CuCxF, 3>::Span;
   using TT = DTensor<float, 3>::Span;
   TT traj;
-  ThreeDPacked(TT t) : traj(t) {};
+  ThreeDPacked(TT t)
+    : traj(t) {};
   void forward(XT x, YT y) const override;
   void adjoint(YT y, XT x) const override;
 };
