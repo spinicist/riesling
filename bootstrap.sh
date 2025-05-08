820f07b04dcf5ea8e437db9268f9244a4e17d837
Tobias Wood <tobias.wood@kcl.ac.uk>
2025-05-08 13:42:05 +0100

    Add Gewurz option

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,14 +5,17 @@ USAGE="Usage: $0 [options]
 Bootstraps the riesling build system (specifies the toolchain for CMake)
 
 Options:
+  -g     Build GEWURZ
   -i DIR Install riesling to this directory, e.g. $HOME/.local
   -j N   Restrict parallel build to this many threads
   -h     Print this message
 "
 PAR=""
 PREFIX=""
-while getopts "f:hi:j:" opt; do
+GEWURZ=""
+while getopts "ghi:j:" opt; do
     case $opt in
+        g) GEWURZ="-DBUILD_GEWURZ=ON";;
         i) PREFIX="-DCMAKE_INSTALL_PREFIX=$OPTARG -DCMAKE_PREFIX_PATH=$OPTARG";;
         j) export VCPKG_MAX_CONCURRENCY=$OPTARG
            PAR="-j $OPTARG";;
@@ -52,7 +55,7 @@ cmake -S . -B build $GEN \
   -DCMAKE_TOOLCHAIN_FILE="${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" \
   -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="-O2 -g -fsanitize=address,undefined" \
   -DVCPKG_INSTALL_OPTIONS="--no-print-usage" \
-  $PREFIX $MONTAGE
+  $PREFIX $MONTAGE $GEWURZ
 cmake --build build $PAR
 
 if [ -n "$PREFIX" ]; then
