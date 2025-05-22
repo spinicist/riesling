6ebc4c98b2441139c74043a4b9e0f08ca6f230b0
Tobias Wood <tobias@spinicist.org.uk>
2025-05-22 15:41:18 +0100

    Reorganise

--- cxx/riesling/op/nufft.cpp
+++ cxx/riesling/op/nufft.cpp
@@ -62,3 +62,62 @@ void main_nufft(args::Subparser &parser)
   }
   Log::Print(cmd, "Finished");
 }
+
+auto Loopify2(typename TOps::NUFFT<2>::Ptr op, Index const nSlice, Index const nTime) -> TOps::TOp<Cx, 6, 5>::Ptr
+{
+  TOps::TOp<Cx, 5, 4>::Ptr sliceLoop = TOps::MakeLoop<2, 3>(op, nSlice);
+  TOps::TOp<Cx, 6, 5>::Ptr timeLoop = TOps::MakeLoop<5, 4>(sliceLoop, nTime);
+  return timeLoop;
+}
+
+void main_nufft2(args::Subparser &parser)
+{
+  CoreArgs<2> coreArgs(parser);
+  GridArgs<2> gridArgs(parser);
+  PreconArgs  preArgs(parser);
+  LSMRArgs    lsqOpts(parser);
+
+  args::Flag fwd(parser, "", "Apply forward operator", {'f', "fwd"});
+  args::Flag adj(parser, "", "Apply adjoint operator", {'a', "adj"});
+
+  ParseCommand(parser, coreArgs.iname, coreArgs.oname);
+  auto const  cmd = parser.GetCommand().Name();
+  HD5::Reader reader(coreArgs.iname.Get());
+
+  TrajectoryN<2> traj(reader, reader.readInfo().voxel_size.head<2>(), coreArgs.matrix.Get());
+  auto const     basis = LoadBasis(coreArgs.basisFile.Get());
+
+  auto const shape = reader.dimensions();
+
+  HD5::Writer writer(coreArgs.oname.Get());
+  writer.writeInfo(reader.readInfo());
+  traj.write(writer);
+
+  if (fwd) {
+    auto const cart = reader.readTensor<Cx6>();
+    auto const nS = shape[2];
+    auto const nC = shape[3];
+    auto const nT = shape[5];
+    auto const nufft = TOps::NUFFT<2>::Make(gridArgs.Get(), traj, nC, basis.get());
+    auto const A = Loopify2(nufft, nS, nT);
+    auto const noncart = A->forward(cart);
+    writer.writeTensor(HD5::Keys::Data, noncart.dimensions(), noncart.data(), HD5::Dims::Noncartesian);
+  } else {
+    auto const noncart = reader.readTensor<Cx5>();
+    auto const nC = shape[0];
+    auto const nS = shape[3];
+    auto const nT = shape[4];
+    auto const nufft = TOps::NUFFT<2>::Make(gridArgs.Get(), traj, nC, basis.get());
+    auto const A = Loopify2(nufft, nS, nT);
+    if (adj) {
+      auto const cart = A->adjoint(noncart);
+      writer.writeTensor(HD5::Keys::Data, cart.dimensions(), cart.data(), HD5::Dims::Channels);
+    } else {
+      auto const M = MakeKSpacePrecon(preArgs.Get(), gridArgs.Get(), traj, nC, Sz2{nS, nT});
+      LSMR const lsmr{A, M, nullptr, lsqOpts.Get()};
+      auto const c = lsmr.run(CollapseToConstVector(noncart));
+      writer.writeTensor(HD5::Keys::Data, A->ishape, c.data(), HD5::Dims::Channels);
+    }
+  }
+  Log::Print(cmd, "Finished");
+}
