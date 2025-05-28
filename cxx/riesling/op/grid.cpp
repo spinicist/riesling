09ce3cd436fe5be076597778dda89302d104ac61
Tobias Wood <tobias@spinicist.org.uk>
2025-05-28 13:29:02 +0100

    Add a 2D grid command

--- cxx/riesling/op/grid.cpp
+++ cxx/riesling/op/grid.cpp
@@ -12,18 +12,25 @@
 
 using namespace rl;
 
+template<int ND>
 auto MakeGrid(
-  GridOpts<3> const &gridOpts, Trajectory const &traj, Index const nC, Index const nS, Index const nT, Basis::CPtr basis)
+  GridOpts<ND> const &gridOpts, TrajectoryN<ND> const &traj, Index const nC, Index const nS, Index const nT, Basis::CPtr basis)
   -> TOps::TOp<Cx, 6, 5>::Ptr
 {
-  auto grid = TOps::Grid<3>::Make(gridOpts, traj, nC, basis);
+  auto G = TOps::Grid<ND>::Make(gridOpts, traj, nC, basis);
+  if constexpr (ND == 2) {
+    auto GS = TOps::MakeLoop<2, 3>(G, nS);
+    auto GST = TOps::MakeLoop<5, 4>(GS, nT);
+    return GST;
+  } else {
   if (nS == 1) {
-    auto rout = TOps::MakeReshapeOutput(grid, AddBack(grid->oshape, 1));
-    auto timeLoop = TOps::MakeLoop<4, 4>(rout, nT);
-    return timeLoop;
+    auto RG = TOps::MakeReshapeOutput(G, AddBack(G->oshape, 1));
+    auto RGT = TOps::MakeLoop<5, 4>(RG, nT);
+    return RGT;
   } else {
     throw(Log::Failure("Recon", "Not supported right now"));
   }
+}
 }
 
 void main_grid(args::Subparser &parser)
@@ -40,6 +47,51 @@ void main_grid(args::Subparser &parser)
   HD5::Reader reader(coreArgs.iname.Get());
 
   Trajectory traj(reader, reader.readInfo().voxel_size, coreArgs.matrix.Get());
+  auto const basis = LoadBasis(coreArgs.basisFile.Get());
+
+  auto const  shape = reader.dimensions();
+  auto const  nC = shape[0];
+  Index const nS = shape[shape.size() - 2];
+  auto const  nT = shape[shape.size() - 1];
+  auto const  A = MakeGrid(gridArgs.Get(), traj, nC, nS, nT, basis.get());
+
+  HD5::Writer writer(coreArgs.oname.Get());
+  writer.writeInfo(reader.readInfo());
+  traj.write(writer);
+
+  if (fwd) {
+    auto const cart = reader.readTensor<Cx6>();
+    auto const noncart = A->forward(cart);
+    writer.writeTensor(HD5::Keys::Data, noncart.dimensions(), noncart.data(), HD5::Dims::Noncartesian);
+  } else if (adj) {
+    auto const noncart = reader.readTensor<Cx5>();
+    auto const cart = A->adjoint(noncart);
+    writer.writeTensor(HD5::Keys::Data, cart.dimensions(), cart.data(), HD5::Dims::Channels);
+  } else {
+    auto const noncart = reader.readTensor<Cx5>();
+    traj.checkDims(FirstN<3>(noncart.dimensions()));
+    auto const M = MakeKSpacePrecon(preArgs.Get(), gridArgs.Get(), traj, nC, Sz2{nS, nT});
+    LSMR const lsmr{A, M, nullptr, lsqOpts.Get()};
+    auto const c = lsmr.run(CollapseToConstVector(noncart));
+    writer.writeTensor(HD5::Keys::Data, A->ishape, c.data(), HD5::Dims::Channels);
+  }
+  Log::Print(cmd, "Finished");
+}
+
+void main_grid2(args::Subparser &parser)
+{
+  CoreArgs<2> coreArgs(parser);
+  GridArgs<2> gridArgs(parser);
+  PreconArgs  preArgs(parser);
+  LSMRArgs    lsqOpts(parser);
+  args::Flag  fwd(parser, "", "Apply forward operation", {'f', "fwd"});
+  args::Flag  adj(parser, "", "Apply adjoint operation", {'a', "adj"});
+
+  ParseCommand(parser, coreArgs.iname, coreArgs.oname);
+  auto const  cmd = parser.GetCommand().Name();
+  HD5::Reader reader(coreArgs.iname.Get());
+
+  TrajectoryN<2> traj(reader, reader.readInfo().voxel_size.head<2>(), coreArgs.matrix.Get());
   auto const basis = LoadBasis(coreArgs.basisFile.Get());
 
   auto const  shape = reader.dimensions();
