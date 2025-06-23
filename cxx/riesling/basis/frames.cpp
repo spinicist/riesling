d86af959454d862d412bf32d97417e79390eb8dd
Tobias Wood <tobias@spinicist.org.uk>
2025-06-23 14:27:10 +0100

    Add interleave mode to frames

--- cxx/riesling/basis/frames.cpp
+++ cxx/riesling/basis/frames.cpp
@@ -15,6 +15,7 @@ void main_frames(args::Subparser &parser)
   args::ValueFlag<Index> startFrame(parser, "F", "Start frame", {"start"});
   args::ValueFlag<Index> incFrame(parser, "F", "Frame increment", {"inc"}, 1);
   args::ValueFlag<Index> retain(parser, "R", "Frames to retain", {"retain"});
+  args::Flag             interleave(parser, "I", "Frames are interleaved", {"interleave", 'i'});
   ParseCommand(parser);
   auto const cmd = parser.GetCommand().Name();
   if (!oname) { throw args::Error("No output filename specified"); }
@@ -25,12 +26,22 @@ void main_frames(args::Subparser &parser)
   rl::Log::Print(cmd, "Frames {} Traces {}", nF, nT);
   rl::Re3 basis(nF, 1, nT);
   basis.setZero();
-  for (Index ifr = 0; ifr < nF; ifr++) {
+  if (interleave) {
     for (Index it = 0; it < tracesPerFrame.Get(); it++) {
-      if (index >= nT) { throw rl::Log::Failure(cmd, "Trace index {} exceeded maximum {}", index, nT); }
-      basis(ifr, 0, index++) = 1.;
+      for (Index ifr = 0; ifr < nF; ifr++) {
+        if (index >= nT) { throw rl::Log::Failure(cmd, "Trace index {} exceeded maximum {}", index, nT); }
+        basis(ifr, 0, index++) = 1.;
+      }
+      if (incFrame) { index += incFrame.Get() * tracesPerFrame.Get(); }
     }
-    if (incFrame) { index += incFrame.Get() * tracesPerFrame.Get(); }
+  } else {
+    for (Index ifr = 0; ifr < nF; ifr++) {
+      for (Index it = 0; it < tracesPerFrame.Get(); it++) {
+        if (index >= nT) { throw rl::Log::Failure(cmd, "Trace index {} exceeded maximum {}", index, nT); }
+        basis(ifr, 0, index++) = 1.;
+      }
+      if (incFrame) { index += incFrame.Get() * tracesPerFrame.Get(); }
+    }
   }
 
   if (retain) { basis = rl::Re3(basis.slice(rl::Sz3{0, 0, 0}, rl::Sz3{retain.Get(), 1, nT})); }
