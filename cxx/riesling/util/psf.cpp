dc459f70373ac62553a7d808571fc67dcfc89a60
Tobias Wood <tobias@spinicist.org.uk>
2025-06-27 14:04:49 +0100

    Add cropping to PSF

--- cxx/riesling/util/psf.cpp
+++ cxx/riesling/util/psf.cpp
@@ -14,33 +14,35 @@ using namespace std::literals::complex_literals;
 
 void main_psf(args::Subparser &parser)
 {
-  CoreArgs<3> coreArgs(parser);
-  GridArgs<3> gridArgs(parser);
-  PreconArgs  preArgs(parser);
-  LSMRArgs    lsqOpts(parser);
+  CoreArgs<3>         coreArgs(parser);
+  GridArgs<3>         gridArgs(parser);
+  PreconArgs          preArgs(parser);
+  LSMRArgs            lsqOpts(parser);
+  ArrayFlag<float, 3> cropFov(parser, "FOV", "Crop FoV in mm (x,y)", {"crop-fov"});
 
   args::Flag tpsf(parser, "T", "Output Transform PSF", {'t', "tpsf"});
   ParseCommand(parser, coreArgs.iname, coreArgs.oname);
-  auto const  cmd = parser.GetCommand().Name();
-  HD5::Reader input(coreArgs.iname.Get());
-  Trajectory  traj(input, input.readStruct<Info>(HD5::Keys::Info).voxel_size, coreArgs.matrix.Get());
-  auto const  basis = LoadBasis(coreArgs.basisFile.Get());
-  auto const  A = TOps::NUFFT<3>::Make(gridArgs.Get(), traj, 1, basis.get());
-  auto const  M = MakeKSpacePrecon(preArgs.Get(), gridArgs.Get(), traj, 1, Sz0{});
-  LSMR const  lsmr{A, M, nullptr, lsqOpts.Get()};
+  auto const       cmd = parser.GetCommand().Name();
+  HD5::Reader      input(coreArgs.iname.Get());
+  Trajectory       traj(input, input.readStruct<Info>(HD5::Keys::Info).voxel_size, coreArgs.matrix.Get());
+  auto const       basis = LoadBasis(coreArgs.basisFile.Get());
+  auto const       A = TOps::NUFFT<3>::Make(gridArgs.Get(), traj, 1, basis.get());
+  auto const       M = MakeKSpacePrecon(preArgs.Get(), gridArgs.Get(), traj, 1, Sz0{});
+  auto const       shape = A->ishape;
+  TOps::Pad<Cx, 5> C(Concatenate(traj.matrixForFOV(cropFov.Get()), LastN<2>(shape)), shape);
+  LSMR const       lsmr{A, M, nullptr, lsqOpts.Get()};
 
   if (tpsf) {
-    auto const  shape = A->ishape;
     Index const nB = shape[4];
     Cx5         imgs(shape);
-    Cx5         output(AddBack(FirstN<3>(shape), nB, nB));
+    Cx5         output(AddBack(FirstN<3>(C.ishape), nB, nB));
 
     for (Index ib = 0; ib < nB; ib++) {
       imgs.setZero();
       imgs(shape[0] / 2, shape[1] / 2, shape[2] / 2, 0, ib) = 1.f;
       Cx3 const ks = A->forward(imgs);
       auto      x = lsmr.run(CollapseToConstVector(ks));
-      output.chip<4>(ib) = AsTensorMap(x, shape).chip<3>(0); // Get rid of channel dimension
+      output.chip<4>(ib) = C.adjoint(AsTensorMap(x, shape)).chip<3>(0); // Get rid of channel dimension
     }
     HD5::Writer writer(coreArgs.oname.Get());
     writer.writeStruct(HD5::Keys::Info, input.readStruct<Info>(HD5::Keys::Info));
@@ -48,10 +50,11 @@ void main_psf(args::Subparser &parser)
   } else {
     Cx3 ks(1, traj.nSamples(), traj.nTraces());
     ks.setConstant(1.f);
-    auto        x = lsmr.run(CollapseToConstVector(ks));
+    auto const  x = lsmr.run(CollapseToConstVector(ks));
+    Cx4 const   out = C.adjoint(AsTensorMap(x, shape)).chip<3>(0);
     HD5::Writer writer(coreArgs.oname.Get());
     writer.writeStruct(HD5::Keys::Info, input.readStruct<Info>(HD5::Keys::Info));
-    writer.writeTensor(HD5::Keys::Data, FirstN<4>(A->ishape), x.data(), {"i", "j", "k", "b"});
+    writer.writeTensor(HD5::Keys::Data, out.dimensions(), out.data(), {"i", "j", "k", "b"});
   }
 
   Log::Print(cmd, "Finished");
