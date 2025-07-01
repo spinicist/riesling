146970a09d89f5f5b2b371f0ddeb5e484d773c32
Tobias Wood <tobias@spinicist.org.uk>
2025-07-02 00:34:49 +0100

    Remove dead declarations

--- cxx/rl/filter.hpp
+++ cxx/rl/filter.hpp
@@ -3,8 +3,6 @@
 #include "types.hpp"
 
 namespace rl {
-void CartesianTukey(float const &start_r, float const &end_r, float const &end_h, Cx4 &x);
-void CartesianTukey(float const &start_r, float const &end_r, float const &end_h, Cx5 &x);
 void NoncartesianTukey(float const &start_r, float const &end_r, float const &end_h, Re3 const &coords, Cx4 &x);
 void NoncartesianTukey(float const &start_r, float const &end_r, float const &end_h, Re3 const &coords, Cx5 &x);
 } // namespace rl
